e9b659d1158c7d8369e113dee9d809429fd386a6
Yuxin Wu <yuxinwu@fb.com>
2019-10-14 17:04:36 -0700

    allow missing groups in GroupedBatchSampler
    Summary:
    Not all groups have samples.
    fix #58
    
    Reviewed By: rbgirshick
    
    Differential Revision: D17909024
    
    fbshipit-source-id: 35367d3c65ce10ea6a90b3df007a7948a4015ea3

---
+++ tests/test_sampler.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import unittest
+from torch.utils.data.sampler import SequentialSampler
+
+from detectron2.data.samplers import GroupedBatchSampler
+
+
+class TestGroupedBatchSampler(unittest.TestCase):
+    def test_missing_group_id(self):
+        sampler = SequentialSampler(list(range(100)))
+        group_ids = [1] * 100
+        s = GroupedBatchSampler(sampler, group_ids, 2)
+
+        for k in s:
+            self.assertEqual(len(k), 2)
+
+    def test_groups(self):
+        sampler = SequentialSampler(list(range(100)))
+        group_ids = [1, 0] * 50
+        s = GroupedBatchSampler(sampler, group_ids, 2)
+
+        for k in s:
+            self.assertTrue((k[0] + k[1]) % 2 == 0)
