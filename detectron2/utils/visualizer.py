9a20df712eaf99ddf85d4286c76b674cfbc69eb3
Yuxin Wu <yuxinwu@fb.com>
2019-10-14 17:04:43 -0700

    support drawing empty masks in visualizer
    Summary: fix #50
    
    Reviewed By: rbgirshick
    
    Differential Revision: D17918494
    
    fbshipit-source-id: 477be945e5901d737c8b8ee5f59f6f74578be7a1

--- detectron2/utils/visualizer.py
+++ detectron2/utils/visualizer.py
@@ -107,6 +107,8 @@ class GenericMask:
         # cv2.CHAIN_APPROX_NONE flag gets vertices of polygons from contours.
         res = cv2.findContours(mask.astype("uint8"), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
         hierarchy = res[-1]
+        if hierarchy is None:  # empty mask
+            return [], False
         has_holes = (hierarchy.reshape(-1, 4)[:, 3] >= 0).sum() > 0
         res = res[-2]
         res = [x.flatten() for x in res]
--- tests/test_visualizer.py
+++ tests/test_visualizer.py
@@ -68,6 +68,18 @@ class TestVisualizer(unittest.TestCase):
         v = Visualizer(img, self.metadata)
         v.draw_instance_predictions(inst)
 
+    def test_draw_empty_mask_predictions(self):
+        img, boxes, _, _, masks = self._random_data()
+        num_inst = len(boxes)
+        inst = Instances((img.shape[0], img.shape[1]))
+        inst.pred_classes = torch.randint(0, 80, size=(num_inst,))
+        inst.scores = torch.rand(num_inst)
+        inst.pred_boxes = torch.from_numpy(boxes)
+        inst.pred_masks = torch.from_numpy(np.zeros_like(np.asarray(masks)))
+
+        v = Visualizer(img, self.metadata)
+        v.draw_instance_predictions(inst)
+
     def test_correct_output_shape(self):
         img = np.random.rand(928, 928, 3) * 255
         v = Visualizer(img, self.metadata)
