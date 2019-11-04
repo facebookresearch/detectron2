bd679de0a9f86bd1db349fe2e19e58fd9080c90a
Roman Shapovalov <romansh@fb.com>
2019-11-04 06:02:08 +0000

    Fix in apply_net.py dump directory.
    It should use full names after listdir.

--- projects/DensePose/apply_net.py
+++ projects/DensePose/apply_net.py
@@ -101,7 +101,7 @@ class InferenceAction(Action):
     def _get_input_file_list(cls: type, input_spec: str):
         if os.path.isdir(input_spec):
             file_list = [
-                fname
+                os.path.join(input_spec, fname)
                 for fname in os.listdir(input_spec)
                 if os.path.isfile(os.path.join(input_spec, fname))
             ]
