ebb9f8c9166765c508f8ac53d9ed2004739b28d1
Yuxin Wu <ppwwyyxx@users.noreply.github.com>
2019-11-10 09:48:06 -0800

    fix visualizer for older opencv (fix #288)
    Summary: Pull Request resolved: https://github.com/fairinternal/detectron2/pull/325
    
    Differential Revision: D18420577
    
    Pulled By: ppwwyyxx
    
    fbshipit-source-id: b0b0f76bccdd68e09a1f68499bbf15cab2476b39

--- detectron2/utils/visualizer.py
+++ detectron2/utils/visualizer.py
@@ -110,6 +110,7 @@ class GenericMask:
         # hierarchy. External contours (boundary) of the object are placed in hierarchy-1.
         # Internal contours (holes) are placed in hierarchy-2.
         # cv2.CHAIN_APPROX_NONE flag gets vertices of polygons from contours.
+        mask = np.ascontiguousarray(mask)  # some versions of cv2 does not support incontiguous arr
         res = cv2.findContours(mask.astype("uint8"), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
         hierarchy = res[-1]
         if hierarchy is None:  # empty mask
