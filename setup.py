0a3025ac83d0ff6a8dd0e210a2713196b9b723e0
Jeremy Fix <jeremy.fix@centralesupelec.fr>
2019-11-11 09:21:08 +0100

    fix: add dependency_links for python setup.py install to work

--- setup.py
+++ setup.py
@@ -70,6 +70,9 @@ setup(
     "platform for object detection and segmentation.",
     packages=find_packages(exclude=("configs", "tests")),
     python_requires=">=3.6",
+    setup_requires=[
+        "cython"
+    ],
     install_requires=[
         "termcolor>=1.1",
         "Pillow",
@@ -79,7 +82,13 @@ setup(
         "matplotlib",
         "tqdm>4.29.0",
         "tensorboard",
+        "cython",
+        "pycocotools @ git+https://github.com/cocodataset/cocoapi.git#subdirectory=PythonAPI",
         "fvcore @ git+https://github.com/facebookresearch/fvcore.git",
+    ],
+    dependency_links=[
+        "git+https://github.com/facebookresearch/fvcore.git@master#egg=fvcore",
+        "git+https://github.com/cocodataset/cocoapi.git#subdirectory=PythonAPI@master#egg=pycocotools"
     ],
     extras_require={"all": ["shapely", "psutil"]},
     ext_modules=get_extensions(),
