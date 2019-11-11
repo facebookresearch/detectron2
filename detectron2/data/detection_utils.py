0e299d254e8bc3f55992e938cbb17db18ffeae90
Yuta Hinokuma <higumachan@users.noreply.github.com>
2019-11-11 16:05:11 +0900

    Update detectron2/data/detection_utils.py
    Co-Authored-By: Yuxin Wu <ppwwyyxxc@gmail.com>

--- detectron2/data/detection_utils.py
+++ detectron2/data/detection_utils.py
@@ -34,7 +34,7 @@ class SizeMismatchError(ValueError):
 def read_image(file_name, format=None):
     """
     Read an image into the given format.
-    If image has exif information then rotate and flipping with that information.
+    Will apply rotation and flipping if the image has such exif information.
 
     Args:
         file_name (str): image file path
