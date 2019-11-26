7ab50f99fce44911f97ffdfaeec4203a0463b6cf
Xinlei Chen <xinleic@fb.com>
2019-11-25 21:25:54 -0800

    TMask: add swap_align2nat op. (#266)
    Summary:
    TensorMask, swap_align2nat op.
    Pull Request resolved: https://github.com/fairinternal/detectron2/pull/266
    
    Reviewed By: ppwwyyxx
    
    Differential Revision: D17179382
    
    Pulled By: endernewton
    
    fbshipit-source-id: d8ae7a835abf56669028b2abd67811bcc7f4787c

---
+++ projects/TensorMask/tensormask/layers/__init__.py
@@ -0,0 +1,4 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+from .swap_align2nat import SwapAlign2Nat, swap_align2nat
+
+__all__ = [k for k in globals().keys() if not k.startswith("_")]
