78cd72d63d5a15fe07de59c7a830201832e7da57
Max Frei <max.frei@uni-due.de>
2019-12-05 22:19:07 -0800

    Only try to load additional config, if args.config_file != "". (#462)
    Summary:
    Fixes https://github.com/facebookresearch/detectron2/issues/461.
    Pull Request resolved: https://github.com/facebookresearch/detectron2/pull/462
    
    Differential Revision: D18828752
    
    Pulled By: ppwwyyxx
    
    fbshipit-source-id: a57193921a7a10983cd3c22319a67c6e395de22a

--- detectron2/engine/defaults.py
+++ detectron2/engine/defaults.py
@@ -104,7 +104,7 @@ def default_setup(cfg, args):
     logger.info("Environment info:\n" + collect_env_info())
 
     logger.info("Command line arguments: " + str(args))
-    if hasattr(args, "config_file"):
+    if hasattr(args, "config_file") and args.config_file != "":
         logger.info(
             "Contents of args.config_file={}:\n{}".format(
                 args.config_file, PathManager.open(args.config_file, "r").read()
