1bff031fabfaf9fac04fc882b0e71400aeea60c2
Ouail <ouail.bendidi@sterblue.com>
2019-12-12 01:32:32 +0100

    use pep517 to install detectron2

--- setup.py
+++ setup.py
@@ -101,6 +101,8 @@ setup(
     python_requires=">=3.6",
     install_requires=[
         "termcolor>=1.1",
+        "torchvision==0.4.2",
+        "torch==1.3.1",
         "Pillow>=6.0",
         "yacs>=0.1.6",
         "tabulate",
