0627aca51fb96b0df1ba3b4808fe81101b744937
Yuxin Wu <yuxinwu@fb.com>
2020-01-06 16:47:33 -0800

    remove deprecated methods from backbone
    Reviewed By: rbgirshick
    
    Differential Revision: D19258472
    
    fbshipit-source-id: a26a92c6eeb8b9193990e69e7c59e7e4e016d20d

--- detectron2/modeling/backbone/backbone.py
+++ detectron2/modeling/backbone/backbone.py
@@ -51,20 +51,3 @@ class Backbone(nn.Module, metaclass=ABCMeta):
             )
             for name in self._out_features
         }
-
-    # the properties below are not used any more
-
-    @property
-    def out_features(self):
-        """deprecated"""
-        return self._out_features
-
-    @property
-    def out_feature_strides(self):
-        """deprecated"""
-        return {f: self._out_feature_strides[f] for f in self._out_features}
-
-    @property
-    def out_feature_channels(self):
-        """deprecated"""
-        return {f: self._out_feature_channels[f] for f in self._out_features}
--- detectron2/modeling/backbone/fpn.py
+++ detectron2/modeling/backbone/fpn.py
@@ -49,8 +49,9 @@ class FPN(Backbone):
         assert isinstance(bottom_up, Backbone)
 
         # Feature map strides and channels from the bottom up network (e.g. ResNet)
-        in_strides = [bottom_up.out_feature_strides[f] for f in in_features]
-        in_channels = [bottom_up.out_feature_channels[f] for f in in_features]
+        input_shapes = bottom_up.output_shape()
+        in_strides = [input_shapes[f].stride for f in in_features]
+        in_channels = [input_shapes[f].channels for f in in_features]
 
         _assert_strides_are_log2_contiguous(in_strides)
         lateral_convs = []
@@ -232,7 +233,7 @@ def build_retinanet_resnet_fpn_backbone(cfg, input_shape: ShapeSpec):
     bottom_up = build_resnet_backbone(cfg, input_shape)
     in_features = cfg.MODEL.FPN.IN_FEATURES
     out_channels = cfg.MODEL.FPN.OUT_CHANNELS
-    in_channels_p6p7 = bottom_up.out_feature_channels["res5"]
+    in_channels_p6p7 = bottom_up.output_shape()["res5"].channels
     backbone = FPN(
         bottom_up=bottom_up,
         in_features=in_features,
