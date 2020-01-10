555200fa414e7cd35ad35a00a3e98f0545f5832e
Milos <m.kondela@me.com>
2020-01-10 14:25:48 -0800

    Refactor test_boxes, test_model_zoo, test_rpn (#672)
    Summary:
    I've noticed that there is some inconsistency between different tests, i.e. some use python's builtin `assert` while others use `unittest.TestCase` asserts (a clear preference is for the latter). I went through `test_boxes.py`, `test_model_zoo.py`, and `test_rpn.py` and refactored it. Also some asserting did not work so I fixed that as well + did some minor refactoring in general.
    Pull Request resolved: https://github.com/facebookresearch/detectron2/pull/672
    
    Differential Revision: D19352228
    
    Pulled By: ppwwyyxx
    
    fbshipit-source-id: 0504324b919584d6dfd9f140ed402bb3e33adc02

--- tests/test_model_zoo.py
+++ tests/test_model_zoo.py
@@ -11,8 +11,8 @@ logger = logging.getLogger(__name__)
 class TestModelZoo(unittest.TestCase):
     def test_get_returns_model(self):
         model = model_zoo.get("Misc/scratch_mask_rcnn_R_50_FPN_3x_gn.yaml", trained=False)
-        assert isinstance(model, GeneralizedRCNN), model
-        assert isinstance(model.backbone, FPN), model.backbone
+        self.assertIsInstance(model, GeneralizedRCNN)
+        self.assertIsInstance(model.backbone, FPN)
 
     def test_get_invalid_model(self):
         self.assertRaises(RuntimeError, model_zoo.get, "Invalid/config.yaml")
