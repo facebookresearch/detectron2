6eee711e76861b66cf49b7063299c8b1a605889a
xmyqsh <xmyqsh@gmail.com>
2020-01-24 19:50:46 +0800

    fix typo

--- detectron2/modeling/meta_arch/panoptic_fpn.py
+++ detectron2/modeling/meta_arch/panoptic_fpn.py
@@ -19,7 +19,7 @@ __all__ = ["PanopticFPN"]
 @META_ARCH_REGISTRY.register()
 class PanopticFPN(nn.Module):
     """
-    Main class for Panoptic FPN architectures (see https://arxiv.org/abd/1901.02446).
+    Main class for Panoptic FPN architectures (see https://arxiv.org/abs/1901.02446).
     """
 
     def __init__(self, cfg):
