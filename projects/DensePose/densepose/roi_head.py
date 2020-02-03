e9293707aa454147ed6a02e6ffd9982a590b8029
Marc Szafraniec <mszafraniec@fb.com>
2020-02-03 06:00:33 -0800

    Make new better parameters default + add Decoder
    Summary: [DensePose] Make new better parameters default + add Decoder
    
    Reviewed By: vkhalidov
    
    Differential Revision: D19580106
    
    fbshipit-source-id: c62c62dd9066ea8d0d6a927e021585d412c81bd3

--- projects/DensePose/densepose/roi_head.py
+++ projects/DensePose/densepose/roi_head.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 
+import numpy as np
+from typing import Dict
+import fvcore.nn.weight_init as weight_init
 import torch
+import torch.nn as nn
+from torch.nn import functional as F
 
+from detectron2.layers import Conv2d, ShapeSpec, get_norm
 from detectron2.modeling import ROI_HEADS_REGISTRY, StandardROIHeads
 from detectron2.modeling.poolers import ROIPooler
 from detectron2.modeling.roi_heads import select_foreground_proposals
@@ -16,6 +22,64 @@ from .densepose_head import (
 )
 
 
+class Decoder(nn.Module):
+    """
+    A semantic segmentation head described in detail in the Panoptic Feature Pyramid Networks paper
+    (https://arxiv.org/abs/1901.02446). It takes FPN features as input and merges information from
+    all levels of the FPN into single output.
+    """
+
+    def __init__(self, cfg, input_shape: Dict[str, ShapeSpec], in_features):
+        super(Decoder, self).__init__()
+
+        # fmt: off
+        self.in_features      = in_features
+        feature_strides       = {k: v.stride for k, v in input_shape.items()}
+        feature_channels      = {k: v.channels for k, v in input_shape.items()}
+        num_classes           = cfg.MODEL.ROI_DENSEPOSE_HEAD.DECODER_NUM_CLASSES
+        conv_dims             = cfg.MODEL.ROI_DENSEPOSE_HEAD.DECODER_CONV_DIMS
+        self.common_stride    = cfg.MODEL.ROI_DENSEPOSE_HEAD.DECODER_COMMON_STRIDE
+        norm                  = cfg.MODEL.ROI_DENSEPOSE_HEAD.DECODER_NORM
+        # fmt: on
+
+        self.scale_heads = []
+        for in_feature in self.in_features:
+            head_ops = []
+            head_length = max(
+                1, int(np.log2(feature_strides[in_feature]) - np.log2(self.common_stride))
+            )
+            for k in range(head_length):
+                conv = Conv2d(
+                    feature_channels[in_feature] if k == 0 else conv_dims,
+                    conv_dims,
+                    kernel_size=3,
+                    stride=1,
+                    padding=1,
+                    bias=not norm,
+                    norm=get_norm(norm, conv_dims),
+                    activation=F.relu,
+                )
+                weight_init.c2_msra_fill(conv)
+                head_ops.append(conv)
+                if feature_strides[in_feature] != self.common_stride:
+                    head_ops.append(
+                        nn.Upsample(scale_factor=2, mode="bilinear", align_corners=False)
+                    )
+            self.scale_heads.append(nn.Sequential(*head_ops))
+            self.add_module(in_feature, self.scale_heads[-1])
+        self.predictor = Conv2d(conv_dims, num_classes, kernel_size=1, stride=1, padding=0)
+        weight_init.c2_msra_fill(self.predictor)
+
+    def forward(self, features):
+        for i, _ in enumerate(self.in_features):
+            if i == 0:
+                x = self.scale_heads[i](features[i])
+            else:
+                x = x + self.scale_heads[i](features[i])
+        x = self.predictor(x)
+        return x
+
+
 @ROI_HEADS_REGISTRY.register()
 class DensePoseROIHeads(StandardROIHeads):
     """
@@ -24,20 +88,28 @@ class DensePoseROIHeads(StandardROIHeads):
 
     def __init__(self, cfg, input_shape):
         super().__init__(cfg, input_shape)
-        self._init_densepose_head(cfg)
+        self._init_densepose_head(cfg, input_shape)
 
-    def _init_densepose_head(self, cfg):
+    def _init_densepose_head(self, cfg, input_shape):
         # fmt: off
         self.densepose_on          = cfg.MODEL.DENSEPOSE_ON
         if not self.densepose_on:
             return
         self.densepose_data_filter = build_densepose_data_filter(cfg)
         dp_pooler_resolution       = cfg.MODEL.ROI_DENSEPOSE_HEAD.POOLER_RESOLUTION
-        dp_pooler_scales           = tuple(1.0 / self.feature_strides[k] for k in self.in_features)
         dp_pooler_sampling_ratio   = cfg.MODEL.ROI_DENSEPOSE_HEAD.POOLER_SAMPLING_RATIO
         dp_pooler_type             = cfg.MODEL.ROI_DENSEPOSE_HEAD.POOLER_TYPE
+        self.use_decoder           = cfg.MODEL.ROI_DENSEPOSE_HEAD.DECODER_ON
+        if self.use_decoder:
+            dp_pooler_scales       = (1.0 / self.feature_strides[self.in_features[0]], )
+        else:
+            dp_pooler_scales       = tuple(1.0 / self.feature_strides[k] for k in self.in_features)
         # fmt: on
         in_channels = [self.feature_channels[f] for f in self.in_features][0]
+
+        if self.use_decoder:
+            self.decoder = Decoder(cfg, input_shape, self.in_features)
+
         self.densepose_pooler = ROIPooler(
             output_size=dp_pooler_resolution,
             scales=dp_pooler_scales,
@@ -72,14 +144,26 @@ class DensePoseROIHeads(StandardROIHeads):
             proposals_dp = self.densepose_data_filter(proposals)
             if len(proposals_dp) > 0:
                 proposal_boxes = [x.proposal_boxes for x in proposals_dp]
-                features_dp = self.densepose_pooler(features, proposal_boxes)
+
+                if self.use_decoder:
+                    features_dec = [self.decoder(features)]
+                else:
+                    features_dec = [features[f] for f in range(len(self.in_features))]
+
+                features_dp = self.densepose_pooler(features_dec, proposal_boxes)
                 densepose_head_outputs = self.densepose_head(features_dp)
                 densepose_outputs, _ = self.densepose_predictor(densepose_head_outputs)
                 densepose_loss_dict = self.densepose_losses(proposals_dp, densepose_outputs)
                 return densepose_loss_dict
         else:
             pred_boxes = [x.pred_boxes for x in instances]
-            features_dp = self.densepose_pooler(features, pred_boxes)
+
+            if self.use_decoder:
+                features_dec = [self.decoder(features)]
+            else:
+                features_dec = [features[f] for f in range(len(self.in_features))]
+
+            features_dp = self.densepose_pooler(features_dec, pred_boxes)
             if len(features_dp) > 0:
                 densepose_head_outputs = self.densepose_head(features_dp)
                 densepose_outputs, _ = self.densepose_predictor(densepose_head_outputs)
