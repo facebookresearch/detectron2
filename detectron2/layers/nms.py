e3145630118ac6e8f954126763a6546521fb1cc0
lbin <lbin@outlook.com>
2020-02-21 19:06:08 +0800

    add amp float func for nms

--- detectron2/layers/nms.py
+++ detectron2/layers/nms.py
@@ -4,8 +4,12 @@
 import torch
 from torchvision.ops import boxes as box_ops
 from torchvision.ops import nms  # BC-compat
+from apex import amp
 
+_nms = amp.float_function(nms)
+_batched_nms = amp.float_function(box_ops.batched_nms)
 
+@amp.float_function
 def batched_nms(boxes, scores, idxs, iou_threshold):
     """
     Same as torchvision.ops.boxes.batched_nms, but safer.
@@ -14,12 +18,12 @@ def batched_nms(boxes, scores, idxs, iou_threshold):
     # TODO may need better strategy.
     # Investigate after having a fully-cuda NMS op.
     if len(boxes) < 40000:
-        return box_ops.batched_nms(boxes, scores, idxs, iou_threshold)
+        return _batched_nms(boxes, scores, idxs, iou_threshold)
 
     result_mask = scores.new_zeros(scores.size(), dtype=torch.bool)
     for id in torch.unique(idxs).cpu().tolist():
         mask = (idxs == id).nonzero().view(-1)
-        keep = nms(boxes[mask], scores[mask], iou_threshold)
+        keep = _nms(boxes[mask], scores[mask], iou_threshold)
         result_mask[mask[keep]] = True
     keep = result_mask.nonzero().view(-1)
     keep = keep[scores[keep].argsort(descending=True)]
