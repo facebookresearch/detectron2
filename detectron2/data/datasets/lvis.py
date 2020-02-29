33e022db2c988ae15c65d9e409711d1693b917d5
Yuxin Wu <yuxinwu@fb.com>
2020-02-28 18:26:28 -0800

    update lvis path
    Reviewed By: alexander-kirillov
    
    Differential Revision: D20175610
    
    fbshipit-source-id: 7f31e934efd6aa703e2f40c30ee9546c5e88bc61

--- detectron2/data/datasets/lvis.py
+++ detectron2/data/datasets/lvis.py
@@ -28,7 +28,7 @@ def register_lvis_instances(name, metadata, json_file, image_root):
         name (str): a name that identifies the dataset, e.g. "lvis_v0.5_train".
         metadata (dict): extra metadata associated with this dataset. It can be an empty dict.
         json_file (str): path to the json instance annotation file.
-        image_root (str): directory which contains all the images.
+        image_root (str or path-like): directory which contains all the images.
     """
     DatasetCatalog.register(name, lambda: load_lvis_json(json_file, image_root, name))
     MetadataCatalog.get(name).set(
