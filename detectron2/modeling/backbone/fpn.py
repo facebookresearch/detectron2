88f659b6bd21c8944878a89e48b6907113d32cfd
corysaildrone <cory@saildrone.com>
2020-03-02 19:04:53 -0800

    Fix typo (double space)

--- detectron2/modeling/backbone/fpn.py
+++ detectron2/modeling/backbone/fpn.py
@@ -129,7 +129,7 @@ class FPN(Backbone):
         for features, lateral_conv, output_conv in zip(
             x[1:], self.lateral_convs[1:], self.output_convs[1:]
         ):
-            # Using F.interpolate with scale_factor instead of size  creates a Gather op in the ONNX model
+            # Using F.interpolate with scale_factor instead of size creates a Gather op in the ONNX model
             # that onnx2trt tool doesn't like. See https://github.com/onnx/onnx-tensorrt/issues/192
             sh = torch.tensor(prev_features.shape)
             top_down_features = F.interpolate(prev_features, size=(prev_features.shape(2) * 2, prev_features.shape(3) * 2), mode="nearest")
