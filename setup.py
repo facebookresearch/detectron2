e6e2a716d95b505d4027ff165c9bba2a08e290f9
tkhe <violin1229@163.com>
2020-04-24 10:59:30 -0700

    update setup.py
    Summary:
    Detectron2 requires PyTorch >= 1.4 now.
    Pull Request resolved: https://github.com/facebookresearch/detectron2/pull/1290
    
    Reviewed By: rbgirshick
    
    Differential Revision: D21225795
    
    Pulled By: ppwwyyxx
    
    fbshipit-source-id: 4402bb36004d3e6cabdd859da9c1c14ef840fab4

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ import torch
 from torch.utils.cpp_extension import CUDA_HOME, CppExtension, CUDAExtension
 
 torch_ver = [int(x) for x in torch.__version__.split(".")[:2]]
-assert torch_ver >= [1, 3], "Requires PyTorch >= 1.3"
+assert torch_ver >= [1, 4], "Requires PyTorch >= 1.4"
 
 
 def get_version():
