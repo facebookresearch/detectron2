6bb5a42af0c97c37fc735e130f72bdf51a2b9f02
David Biertimpel <david.biertimpel@protonmail.com>
2020-04-24 12:19:52 -0700

    Adding histograms to tensorboard
    Summary:
    Hey,
    
    as this is a small PR and I already implemented this feature for myself I did not open an issue.
    
    Description:
    I changed EventStorage and TensorboardXWriter such that histograms (for monitoring weights or gradients) can be saved to tensorboard.
    
    To make this implementation as efficient as possible I calculate the histogram statistics for the add_histogram_raw function of the PyTorch SummaryWriter (instead of saving weight matrices). In general, I tried to match the information flow to that of the put_image function.
    
    In my personal implementation I extended the EventStorage and TensorboardXWriter, but I thought that this might be a feature that Detectron could offer out of the box.
    
    A possible example usage could be a Hook:
    ```
    class HistogramHook(HookBase):
        def after_step(self):
            storage = self.trainer.storage
            hist_period = self.trainer.cfg.HIST_PERIOD
    
            if hist_period > 0 and storage.iter % hist_period == 0:
                self.add_histogram(self.trainer.model, storage)
    
        def add_histogram(self, model, storage):
            for i, (tag, value) in enumerate(model.named_parameters()):
                # E.g. add histograms for parameters of the Mask R-CNN mask head
                if tag.split(".")[1] == "mask_head":
                    storage.put_histogram(tag, value.data.cpu().numpy())
    ```
    Feedback is very much appreciated! :)
    
    Best,
    David
    Pull Request resolved: https://github.com/facebookresearch/detectron2/pull/1265
    
    Reviewed By: rbgirshick
    
    Differential Revision: D21223968
    
    Pulled By: ppwwyyxx
    
    fbshipit-source-id: 92a7b3d04fd1b45b4fd35fb9a814e529ba40ab9d

--- detectron2/utils/events.py
+++ detectron2/utils/events.py
@@ -136,6 +136,11 @@ class TensorboardXWriter(EventWriter):
                 self._writer.add_image(img_name, img, step_num)
             storage.clear_images()
 
+        if len(storage.hist_data) >= 1:
+            for params in storage.hist_data:
+                self._writer.add_histogram_raw(**params)
+            storage.clear_histograms()
+
     def close(self):
         if hasattr(self, "_writer"):  # doesn't exist when the code fails at import
             self._writer.close()
@@ -235,6 +240,7 @@ class EventStorage:
         self._iter = start_iter
         self._current_prefix = ""
         self._vis_data = []
+        self._histograms = []
 
     def put_image(self, img_name, img_tensor):
         """
@@ -295,6 +301,43 @@ class EventStorage:
         for k, v in kwargs.items():
             self.put_scalar(k, v, smoothing_hint=smoothing_hint)
 
+    def put_histogram(self, hist_name, hist_tensor, bins=1000):
+        """
+        Create a histogram from a tensor.
+
+        Args:
+            hist_name (str): The name of the histogram to put into tensorboard.
+            hist_tensor (torch.Tensor): A Tensor of arbitrary shape to be converted
+                into a histogram.
+            bins (int): Number of histogram bins.
+        """
+        ht_min, ht_max = hist_tensor.min().item(), hist_tensor.max().item()
+
+        # Create a histogram with PyTorch
+        hist_counts = torch.histc(hist_tensor, bins=bins)
+        hist_edges = torch.linspace(start=ht_min, end=ht_max, steps=bins + 1, dtype=torch.float32)
+
+        # Parameter for the add_histogram_raw function of SummaryWriter
+        hist_params = dict(
+            tag=hist_name,
+            min=ht_min,
+            max=ht_max,
+            num=len(hist_tensor),
+            sum=float(hist_tensor.sum()),
+            sum_squares=float(torch.sum(hist_tensor ** 2)),
+            bucket_limits=hist_edges[1:].tolist(),
+            bucket_counts=hist_counts.tolist(),
+            global_step=self._iter,
+        )
+        self._histograms.append(hist_params)
+
+    def clear_histograms(self):
+        """
+        Delete all the stored histograms for visualization.
+        This should be called after histograms are written to tensorboard.
+        """
+        self._histograms = []
+
     def history(self, name):
         """
         Returns:
@@ -354,6 +397,10 @@ class EventStorage:
     @property
     def vis_data(self):
         return self._vis_data
+
+    @property
+    def hist_data(self):
+        return self._histograms
 
     @property
     def iter(self):
