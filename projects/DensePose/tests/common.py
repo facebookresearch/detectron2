4915330cac34570fc2ea4fa2c8b4088de448a973
Patrick Labatut <plabatut@fb.com>
2020-04-28 05:00:29 -0700

    Add unit tests for loading evolution configs
    Summary: Add unit tests for loading evolution configs. Changed logic to find configs to validate (consider only regular files and skip `.yaml` files with `Base-` prefix)
    
    Reviewed By: vkhalidov
    
    Differential Revision: D21276063
    
    fbshipit-source-id: 57b1439013581bfbe40c334a7840fd2350bf72c4

--- projects/DensePose/tests/common.py
+++ projects/DensePose/tests/common.py
@@ -7,11 +7,12 @@ from detectron2.config import get_cfg
 from detectron2.engine import default_setup
 from detectron2.modeling import build_model
 
-from densepose import add_densepose_config
+from densepose import add_dataset_category_config, add_densepose_config
 
 _BASE_CONFIG_DIR = "configs"
+_EVOLUTION_CONFIG_SUB_DIR = "evolution"
 _QUICK_SCHEDULES_CONFIG_SUB_DIR = "quick_schedules"
-_CONFIG_FILE_PREFIX = "densepose_"
+_BASE_CONFIG_FILE_PREFIX = "Base-"
 _CONFIG_FILE_EXT = ".yaml"
 
 
@@ -20,6 +21,13 @@ def _get_base_config_dir():
     Return the base directory for configurations
     """
     return os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", _BASE_CONFIG_DIR)
+
+
+def _get_evolution_config_dir():
+    """
+    Return the base directory for evolution configurations
+    """
+    return os.path.join(_get_base_config_dir(), _EVOLUTION_CONFIG_SUB_DIR)
 
 
 def _get_quick_schedules_config_dir():
@@ -36,12 +44,14 @@ def _collect_config_files(config_dir):
     start = _get_base_config_dir()
     results = []
     for entry in os.listdir(config_dir):
+        path = os.path.join(config_dir, entry)
+        if not os.path.isfile(path):
+            continue
         _, ext = os.path.splitext(entry)
         if ext != _CONFIG_FILE_EXT:
             continue
-        if not entry.startswith(_CONFIG_FILE_PREFIX):
+        if entry.startswith(_BASE_CONFIG_FILE_PREFIX):
             continue
-        path = os.path.join(config_dir, entry)
         config_file = os.path.relpath(path, start)
         results.append(config_file)
     return results
@@ -52,6 +62,13 @@ def get_config_files():
     Get all the configuration files (relative to the base configuration directory)
     """
     return _collect_config_files(_get_base_config_dir())
+
+
+def get_evolution_config_files():
+    """
+    Get all the evolution configuration files (relative to the base configuration directory)
+    """
+    return _collect_config_files(_get_evolution_config_dir())
 
 
 def get_quick_schedules_config_files():
@@ -67,6 +84,7 @@ def _get_model_config(config_file):
     directory)
     """
     cfg = get_cfg()
+    add_dataset_category_config(cfg)
     add_densepose_config(cfg)
     path = os.path.join(_get_base_config_dir(), config_file)
     cfg.merge_from_file(path)
