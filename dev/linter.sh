1f6ebff69b79f93d69e59eca9c2e84a9f03d850e
Yuxin Wu <yuxinwu@fb.com>
2020-05-08 13:51:57 -0700

    update linter version check
    Reviewed By: alexander-kirillov
    
    Differential Revision: D21480696
    
    fbshipit-source-id: 72658ad2797aecf1196d3323991701a5bfb7e1e5

--- dev/linter.sh
+++ dev/linter.sh
@@ -8,7 +8,7 @@ vergte() {
 }
 
 {
-  black --version | grep -E "(19.3b0.*6733274)|(19.3b0.*db6ed34)" > /dev/null
+  black --version | grep -E "(19.3b0.*6733274)|(19.3b0\\+8)" > /dev/null
 } || {
 	echo "Linter requires 'black @ git+https://github.com/psf/black@673327449f86fce558adde153bb6cbe54bfebad2' !"
 	exit 1
