c553bf461163cd0fd9bf8322febbd848c85b5579
Yuxin Wu <yuxinwu@fb.com>
2020-05-11 20:29:22 -0700

    fix bug in TRAIN_ON_PRED_BOXES
    Summary: fix https://github.com/facebookresearch/detectron2/issues/1400
    
    Reviewed By: rbgirshick
    
    Differential Revision: D21517395
    
    fbshipit-source-id: 3bd2ada130515b1dbdb06c3eb01ef3d72283b85d

--- detectron2/modeling/roi_heads/roi_heads.py
+++ detectron2/modeling/roi_heads/roi_heads.py
@@ -651,6 +651,8 @@ class StandardROIHeads(ROIHeads):
         del box_features
 
         if self.training:
+            losses = self.box_predictor.losses(predictions, proposals)
+            # proposals is modified in-place below, so losses must be computed first.
             if self.train_on_pred_boxes:
                 with torch.no_grad():
                     pred_boxes = self.box_predictor.predict_boxes_for_gt_classes(
@@ -658,7 +660,7 @@ class StandardROIHeads(ROIHeads):
                     )
                     for proposals_per_image, pred_boxes_per_image in zip(proposals, pred_boxes):
                         proposals_per_image.proposal_boxes = Boxes(pred_boxes_per_image)
-            return self.box_predictor.losses(predictions, proposals)
+            return losses
         else:
             pred_instances, _ = self.box_predictor.inference(predictions, proposals)
             return pred_instances
--- docs/notes/changelog.md
+++ docs/notes/changelog.md
@@ -20,6 +20,7 @@ See release log at
 
 We list a few silent regressions since they may silently produce incorrect results and will be hard to debug.
 
+* 04/01/2020 - 05/11/2020: Bad accuracy if `TRAIN_ON_PRED_BOXES` is set to True.
 * 03/30/2020 - 04/01/2020: ResNets are not correctly built.
 * 12/19/2019 - 12/26/2019: Using aspect ratio grouping causes a drop in accuracy.
 * release - 11/9/2019: Test time augmentation does not predict the last category.
