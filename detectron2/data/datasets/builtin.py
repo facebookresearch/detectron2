30407a6e6b0f6580ee8e8ddf479738b22dd593df
Pakorn Vongseela <18078609+pakornvs@users.noreply.github.com>
2020-06-18 23:29:00 +0700

    Undo builtin.py

--- detectron2/data/datasets/builtin.py
+++ detectron2/data/datasets/builtin.py
@@ -4,12 +4,15 @@
 
 """
 This file registers pre-defined datasets at hard-coded paths, and their metadata.
+
 We hard-code metadata for common datasets. This will enable:
 1. Consistency check when loading the datasets
 2. Use models on these standard datasets directly and run demos,
    without having to download the dataset annotations
+
 We hard-code some paths to the dataset that's assumed to
 exist in "./datasets/".
+
 Users SHOULD NOT use this file to create new dataset / metadata for new dataset.
 To add new dataset, refer to the tutorial "docs/DATASETS.md".
 """
