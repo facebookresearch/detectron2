92f85b57dbcd7231b368b66dfa8bb6727b435aac
Yuxin Wu <yuxinwu@fb.com>
2020-06-17 19:34:03 -0700

    move concrete TransformGen implementations to a separate file
    Summary: prepare for more code in transform_gen.py
    
    Reviewed By: rbgirshick
    
    Differential Revision: D22068356
    
    fbshipit-source-id: cb661ee57289515064b0ea71ce185f9f854d3254

--- detectron2/data/transforms/__init__.py
+++ detectron2/data/transforms/__init__.py
@@ -2,5 +2,6 @@
 from .transform import *
 from fvcore.transforms.transform import *
 from .transform_gen import *
+from .transform_gen_impl import *
 
 __all__ = [k for k in globals().keys() if not k.startswith("_")]
