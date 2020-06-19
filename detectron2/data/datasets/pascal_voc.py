e845b407358af1a7c54bcd528b4a20e0ff26efa2
Pakorn Vongseela <18078609+pakornvs@users.noreply.github.com>
2020-06-19 00:30:15 -0700

    Allow registering pascal voc dataset with custom class names
    Summary:
    Relate: https://github.com/facebookresearch/detectron2/issues/991
    Pull Request resolved: https://github.com/facebookresearch/detectron2/pull/1603
    
    Differential Revision: D22118724
    
    Pulled By: ppwwyyxx
    
    fbshipit-source-id: c6ad85e8f250345cf2e2abf512323ae1166051ae

--- detectron2/data/datasets/pascal_voc.py
+++ detectron2/data/datasets/pascal_voc.py
@@ -4,30 +4,32 @@
 import numpy as np
 import os
 import xml.etree.ElementTree as ET
+from typing import List, Tuple, Union
 from fvcore.common.file_io import PathManager
 
 from detectron2.data import DatasetCatalog, MetadataCatalog
 from detectron2.structures import BoxMode
 
-__all__ = ["register_pascal_voc"]
+__all__ = ["load_voc_instances", "register_pascal_voc"]
 
 
 # fmt: off
-CLASS_NAMES = [
+CLASS_NAMES = (
     "aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat",
     "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person",
-    "pottedplant", "sheep", "sofa", "train", "tvmonitor",
-]
+    "pottedplant", "sheep", "sofa", "train", "tvmonitor"
+)
 # fmt: on
 
 
-def load_voc_instances(dirname: str, split: str):
+def load_voc_instances(dirname: str, split: str, class_names: Union[List[str], Tuple[str, ...]]):
     """
     Load Pascal VOC detection annotations to Detectron2 format.
 
     Args:
         dirname: Contain "Annotations", "ImageSets", "JPEGImages"
         split (str): one of "train", "test", "val", "trainval"
+        class_names: list or tuple of class names
     """
     with PathManager.open(os.path.join(dirname, "ImageSets", "Main", split + ".txt")) as f:
         fileids = np.loadtxt(f, dtype=np.str)
@@ -66,15 +68,15 @@ def load_voc_instances(dirname: str, split: str):
             bbox[0] -= 1.0
             bbox[1] -= 1.0
             instances.append(
-                {"category_id": CLASS_NAMES.index(cls), "bbox": bbox, "bbox_mode": BoxMode.XYXY_ABS}
+                {"category_id": class_names.index(cls), "bbox": bbox, "bbox_mode": BoxMode.XYXY_ABS}
             )
         r["annotations"] = instances
         dicts.append(r)
     return dicts
 
 
-def register_pascal_voc(name, dirname, split, year):
-    DatasetCatalog.register(name, lambda: load_voc_instances(dirname, split))
+def register_pascal_voc(name, dirname, split, year, class_names=CLASS_NAMES):
+    DatasetCatalog.register(name, lambda: load_voc_instances(dirname, split, class_names))
     MetadataCatalog.get(name).set(
-        thing_classes=CLASS_NAMES, dirname=dirname, year=year, split=split
+        thing_classes=list(class_names), dirname=dirname, year=year, split=split
     )
