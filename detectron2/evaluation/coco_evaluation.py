d4829a673976b83cb949b827bb151d9e77103862
Steve Branson <stevebranson@fb.com>
2020-06-22 10:13:59 -0700

    Speedup COCO evaluation
    Summary:
    Implementation of COCOeval.evaluate() and COCOeval.accumulate()  in c++.
    The c++ evaluate() function is similar speed to Google's implementation, but the c++ accumulate() function is slower (I haven't looked into why, but it's not currently the bottleneck)
    Not implementing IOUs in c++ is currently the bottleneck of this implementation
    There is some unknown time overhead of a couple of seconds in calling c++ functions from python (maybe something is being implicitly converted).
    On the COCO validation set, precision outputs exactly match the PythonAPI (the Google version is off by around 1e-4)
    On the COCO trainset, computational speedup and precision comparisons are similar
    
    Reviewed By: ppwwyyxx
    
    Differential Revision: D21406448
    
    fbshipit-source-id: 137500f58c5375be68215a78c8400820d668844b

--- detectron2/evaluation/coco_evaluation.py
+++ detectron2/evaluation/coco_evaluation.py
@@ -13,12 +13,12 @@ import pycocotools.mask as mask_util
 import torch
 from fvcore.common.file_io import PathManager
 from pycocotools.coco import COCO
-from pycocotools.cocoeval import COCOeval
 from tabulate import tabulate
 
 import detectron2.utils.comm as comm
 from detectron2.data import MetadataCatalog
 from detectron2.data.datasets.coco import convert_to_coco_json
+from detectron2.evaluation.fast_eval_api import COCOeval_opt as COCOeval
 from detectron2.structures import Boxes, BoxMode, pairwise_iou
 from detectron2.utils.logger import create_small_table
 
