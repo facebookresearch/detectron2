54d2ffb28280d713d65835a169a297f78843e751
cdisch <constantin.disch@mevis.fraunhofer.de>
2020-07-15 21:35:55 +0200

    Bitmask bounding boxes: Add test only for bounding box

---
+++ tests/structures/test_masks.py
@@ -0,0 +1,35 @@
+import unittest
+from detectron2.structures.masks import BitMasks
+import torch
+
+
+class TestBitMask(unittest.TestCase):
+
+    def test_get_bounding_box(self):
+        mask = torch.tensor([
+                [
+                    [False, False, False, True],
+                    [False, False, True,  True],
+                    [False, True,  True,  False],
+                    [False, True,  True,  False],
+                    [False, False, False, False]
+                ],
+                [
+                    [False, False, False, False],
+                    [False, False, False, False],
+                    [False, False, False, False],
+                    [False, False, False, False],
+                    [False, False, False, False]
+                ]
+            ]
+        )
+        bitmask = BitMasks(mask)
+        box_true = torch.tensor([[1, 0, 3, 3], [0, 0, 0, 0]], dtype=torch.float32)
+        box = bitmask.get_bounding_boxes()
+        self.assertTrue(torch.all(box.tensor == box_true))
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
