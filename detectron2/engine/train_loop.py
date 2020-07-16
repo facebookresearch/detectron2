f41099aa6f31aa0b7b82937f0da3ec7d5124656f
Yuxin Wu <yuxinwu@fb.com>
2020-07-15 17:09:14 -0700

    trainer should allow CPU training
    Reviewed By: newstzpz
    
    Differential Revision: D22527742
    
    fbshipit-source-id: 0e295ee646336d1b94fcc1bbdb79284eb42483c9

--- detectron2/engine/train_loop.py
+++ detectron2/engine/train_loop.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 
+import contextlib
 import logging
 import numpy as np
 import time
@@ -11,6 +12,15 @@ import detectron2.utils.comm as comm
 from detectron2.utils.events import EventStorage
 
 __all__ = ["HookBase", "TrainerBase", "SimpleTrainer"]
+
+
+try:
+    _nullcontext = contextlib.nullcontext  # python 3.7+
+except AttributeError:
+
+    @contextlib.contextmanager
+    def _nullcontext(enter_result=None):
+        yield enter_result
 
 
 class HookBase:
@@ -224,7 +234,9 @@ class SimpleTrainer(TrainerBase):
         losses.backward()
 
         # use a new stream so the ops don't wait for DDP
-        with torch.cuda.stream(torch.cuda.Stream()):
+        with torch.cuda.stream(
+            torch.cuda.Stream()
+        ) if losses.device.type == "cuda" else _nullcontext():
             metrics_dict = loss_dict
             metrics_dict["data_time"] = data_time
             self._write_metrics(metrics_dict)
