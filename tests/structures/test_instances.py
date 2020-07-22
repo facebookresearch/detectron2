b38b57ad27d69b272c016f1a3759597e89cfa159
chenbohua3 <chen_bohua@hotmail.com>
2020-07-22 10:15:08 +0800

    Add new_fields test.

--- tests/structures/test_instances.py
+++ tests/structures/test_instances.py
@@ -2,7 +2,9 @@
 import unittest
 import torch
 
+from detectron2.export.patch_instance import patch_instance
 from detectron2.structures import Instances
+from detectron2.utils.env import TORCH_VERSION
 
 
 class TestInstancesIndexing(unittest.TestCase):
@@ -19,6 +21,37 @@ class TestInstancesIndexing(unittest.TestCase):
 
         self.assertRaises(IndexError, lambda: instances[len(instances)])
         self.assertRaises(IndexError, lambda: instances[-len(instances) - 1])
+
+
+class TestInstancesScriptability(unittest.TestCase):
+    @unittest.skipIf(TORCH_VERSION < (1, 6), "Insufficient pytorch version")
+    def test_new_fields(self):
+        fields = {"proposal_boxes": "Boxes", "objectness_logits": "Tensor"}
+
+        class f(torch.nn.Module):
+            def forward(self, x: Instances):
+                proposal_boxes = x.proposal_boxes  # noqa F841
+                objectness_logits = x.objectness_logits  # noqa F841
+                return x
+
+        class g(torch.nn.Module):
+            def forward(self, x: Instances):
+                mask = x.mask  # noqa F841
+                return x
+
+        class g2(torch.nn.Module):
+            def forward(self, x: Instances):
+                proposal_boxes = x.proposal_boxes  # noqa F841
+                return x
+
+        with patch_instance(fields):
+            torch.jit.script(f())
+
+        new_fields = {"mask": "Tensor"}
+        with patch_instance(new_fields):
+            torch.jit.script(g())
+            with self.assertRaises(Exception):
+                torch.jit.script(g2())
 
 
 if __name__ == "__main__":
