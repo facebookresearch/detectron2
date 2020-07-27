b03bbd7df056340c69fb4164c90088cf00835e2d
Bowen Cheng <bowenc0221@gmail.com>
2020-07-26 20:05:57 -0700

    Reproduce DeepLabV3 and DeepLabV3+ in Detectron2.
    Summary:
    In this PR, I implemented DeepLabV3 and DeepLabV3+ in Detectron2. These two models can be served as strong baselines for semantic segmentation. The following table shows results on Cityscapes val set.
    
    All results are median of 5 runs.
    
    | Method             | Backbone | Input size | Color | mIoU (our)| mIoU (paper) |
    |----------------|-------------|------------|--------------------|-----------|------------------|
    | DeepLabV3      | R101-DC5 | 1024x2048| no                             |  77.6          | 77.2                      |
    | DeepLabV3      | R101-DC45 | 1024x2048| no                             |  78.7          | N/A                      |
    | DeepLabV3      | R101-DC5 | 1024x2048| yes                            |  78.2         | N/A                      |
    | DeepLabV3      | R101-DC45 | 1024x2048| yes                            |  79.9         | N/A                      |
    | DeepLabV3+    | R101-DC5 | 1024x2048| no                             |  78.5         | 78.8*                    |
    | DeepLabV3+    | R101-DC45 | 1024x2048| no                             |  78.7         | N/A                    |
    | DeepLabV3+    | R101-DC5 | 1024x2048| yes                            |  79.6         | N/A                      |
    | DeepLabV3+    | R101-DC45 | 1024x2048| yes                            |  80.2         | N/A                      |
    
    DC5: dilated res5
    DC45: dilated res4 and res5
    Color: SSD color augmentation
    *Note: * indicates result using Xception-65 as backbone.*
    
    Pull Request resolved: https://github.com/fairinternal/detectron2/pull/414
    
    Test Plan:
    The code is tested on FAIR Cluster with 8 GPUs using the following command:
    ```
    python3 d2_submitit.py --config configs/Cityscapes-SemanticSegmentation/deeplab_v3_R_101_os16_mg124_poly_90k_bs16.yaml --num-gpus 8 --resume
    ```
    
    References:
    *   DeepLabV3+:
    
    ```
    inproceedings{deeplabv3plus2018,
      title={Encoder-Decoder with Atrous Separable Convolution for Semantic Image Segmentation},
      author={Liang-Chieh Chen and Yukun Zhu and George Papandreou and Florian Schroff and Hartwig Adam},
      booktitle={ECCV},
      year={2018}
    }
    ```
    
    *   DeepLabV3:
    
    ```
    article{deeplabv32018,
      title={Rethinking atrous convolution for semantic image segmentation},
      author={Chen, Liang-Chieh and Papandreou, George and Schroff, Florian and Adam, Hartwig},
      journal={arXiv:1706.05587},
      year={2017}
    }
    ```
    
    Reviewed By: ppwwyyxx
    
    Differential Revision: D22757071
    
    Pulled By: bowenc0221
    
    fbshipit-source-id: 61002c8a7f8a2fb11bebf5cf96966f79e5531edf

---
+++ projects/DeepLab/deeplab/build_solver.py
@@ -0,0 +1,28 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+import torch
+
+from detectron2.config import CfgNode
+from detectron2.solver import build_lr_scheduler as build_d2_lr_scheduler
+
+from .lr_scheduler import WarmupPolyLR
+
+
+def build_lr_scheduler(
+    cfg: CfgNode, optimizer: torch.optim.Optimizer
+) -> torch.optim.lr_scheduler._LRScheduler:
+    """
+    Build a LR scheduler from config.
+    """
+    name = cfg.SOLVER.LR_SCHEDULER_NAME
+    if name == "WarmupPolyLR":
+        return WarmupPolyLR(
+            optimizer,
+            cfg.SOLVER.MAX_ITER,
+            warmup_factor=cfg.SOLVER.WARMUP_FACTOR,
+            warmup_iters=cfg.SOLVER.WARMUP_ITERS,
+            warmup_method=cfg.SOLVER.WARMUP_METHOD,
+            power=cfg.SOLVER.POLY_LR_POWER,
+            constant_ending=cfg.SOLVER.POLY_LR_CONSTANT_ENDING,
+        )
+    else:
+        return build_d2_lr_scheduler(cfg, optimizer)
