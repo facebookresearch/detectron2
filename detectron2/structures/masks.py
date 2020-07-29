1a9f63170c61e87767d0948d8f30234dd09888af
consti <39835850+constiDisch@users.noreply.github.com>
2020-07-29 17:24:06 +0200

    Update documentation for tight bounding boxes
    Co-authored-by: Yuxin Wu <ppwwyyxxc@gmail.com>

--- detectron2/structures/masks.py
+++ detectron2/structures/masks.py
@@ -202,7 +202,7 @@ class BitMasks:
     def get_bounding_boxes(self) -> Boxes:
         """
         Returns:
-            Boxes: tight bounding boxes around bit masks.
+            Boxes: tight bounding boxes around bitmasks. If a mask is empty, it's bounding box will be all zero.
         """
         boxes = torch.zeros(self.tensor.shape[0], 4, dtype=torch.float32)
         x_any = torch.any(self.tensor, dim=1)
