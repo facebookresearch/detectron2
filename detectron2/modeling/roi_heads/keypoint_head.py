7641de4facfb8205b01f4d1c1d713ba6dfec7993
Jon Morton <jamorton@fb.com>
2020-08-24 19:16:18 -0700

    save heatmaps for later use in keypoint_rcnn_inference
    Summary:
    It can be useful to have access to the raw heatmaps in the post-processing of keypoint model outputs. This diff adds `pred_keypoint_heatmaps` field to the keypoint instances returns by `keypoint_rcnn_inference` so that they are accessible outside of the keypoint head.
    
    This makes it easy to do custom post processing without changing around the whole vanilla heatmap->keypoint path.
    
    Reviewed By: ppwwyyxx
    
    Differential Revision: D23183989
    
    fbshipit-source-id: 50c9b44545f090ae95eaa2b70ce70e8f04d8d0e6

--- detectron2/modeling/roi_heads/keypoint_head.py
+++ detectron2/modeling/roi_heads/keypoint_head.py
@@ -108,21 +108,28 @@ def keypoint_rcnn_inference(pred_keypoint_logits, pred_instances):
         pred_instances (list[Instances]): A list of N Instances, where N is the number of images.
 
     Returns:
-        None. Each element in pred_instances will contain an extra "pred_keypoints" field.
-            The field is a tensor of shape (#instance, K, 3) where the last
-            dimension corresponds to (x, y, score).
-            The scores are larger than 0.
+        None. Each element in pred_instances will contain extra "pred_keypoints" and
+            "pred_keypoint_heatmaps" fields. "pred_keypoints" is a tensor of shape
+            (#instance, K, 3) where the last dimension corresponds to (x, y, score).
+            The scores are larger than 0. "pred_keypoint_heatmaps" contains the raw
+            keypoint logits as passed to this function.
     """
     # flatten all bboxes from all images together (list[Boxes] -> Rx4 tensor)
     bboxes_flat = cat([b.pred_boxes.tensor for b in pred_instances], dim=0)
 
-    keypoint_results = heatmaps_to_keypoints(pred_keypoint_logits.detach(), bboxes_flat.detach())
+    pred_keypoint_logits = pred_keypoint_logits.detach()
+    keypoint_results = heatmaps_to_keypoints(pred_keypoint_logits, bboxes_flat.detach())
     num_instances_per_image = [len(i) for i in pred_instances]
     keypoint_results = keypoint_results[:, :, [0, 1, 3]].split(num_instances_per_image, dim=0)
+    heatmap_results = pred_keypoint_logits.split(num_instances_per_image, dim=0)
 
-    for keypoint_results_per_image, instances_per_image in zip(keypoint_results, pred_instances):
+    for keypoint_results_per_image, heatmap_results_per_image, instances_per_image in zip(
+        keypoint_results, heatmap_results, pred_instances
+    ):
         # keypoint_results_per_image is (num instances)x(num keypoints)x(x, y, score)
+        # heatmap_results_per_image is (num instances)x(num keypoints)x(side)x(side)
         instances_per_image.pred_keypoints = keypoint_results_per_image
+        instances_per_image.pred_keypoint_heatmaps = heatmap_results_per_image
 
 
 class BaseKeypointRCNNHead(nn.Module):
