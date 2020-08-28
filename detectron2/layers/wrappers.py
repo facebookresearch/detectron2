4fdb72998d39f46ef19c0a1ec891e24b8a53cc37
meetgandhi123 <meet.gandhi1415@gmail.com>
2020-08-28 11:18:21 +0530

    Issue: 1916 UserWarning: This overload of nonzero is deprecated:

--- detectron2/layers/wrappers.py
+++ detectron2/layers/wrappers.py
@@ -223,4 +223,4 @@ def nonzero_tuple(x):
     """
     if x.dim() == 0:
         return x.unsqueeze(0).nonzero().unbind(1)
-    return x.nonzero().unbind(1)
+    return torch.nonzero(x, as_tuple=True)
