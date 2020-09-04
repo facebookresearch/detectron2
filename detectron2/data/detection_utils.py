eef6898f2d06b2cf7de48b93ae017cbba276b8e8
Milos <m.kondela@me.com>
2020-09-04 08:55:46 +0200

    Change checking of config based on code review

--- detectron2/data/detection_utils.py
+++ detectron2/data/detection_utils.py
@@ -579,11 +579,11 @@ def build_augmentation(cfg, is_train):
         max_size = cfg.INPUT.MAX_SIZE_TEST
         sample_style = "choice"
     augmentation = [T.ResizeShortestEdge(min_size, max_size, sample_style)]
-    if is_train and "none" not in cfg.INPUT.RANDOM_FLIP:
+    if is_train and cfg.INPUT.RANDOM_FLIP != "none":
         augmentation.append(
             T.RandomFlip(
-                horizontal=True if "horizontal" in cfg.INPUT.RANDOM_FLIP else False,
-                vertical=True if "vertical" in cfg.INPUT.RANDOM_FLIP else False,
+                horizontal=cfg.INPUT.RANDOM_FLIP == "horizontal",
+                vertical=cfg.INPUT.RANDOM_FLIP == "vertical",
             )
         )
     return augmentation
