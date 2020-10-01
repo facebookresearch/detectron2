9c514cd3fb997044ecfba90f52a8d1c99dcbad78
Yuxin Wu <yuxinwu@fb.com>
2020-09-30 17:39:40 -0700

    use @jit.unused to ignore properties
    Reviewed By: alexander-kirillov
    
    Differential Revision: D24031545
    
    fbshipit-source-id: b0c6ee11e83434c4b68008ebb6ab059ed696f226

--- detectron2/modeling/roi_heads/box_head.py
+++ detectron2/modeling/roi_heads/box_head.py
@@ -2,6 +2,7 @@
 import numpy as np
 from typing import List
 import fvcore.nn.weight_init as weight_init
+import torch
 from torch import nn
 
 from detectron2.config import configurable
@@ -27,8 +28,6 @@ class FastRCNNConvFCHead(nn.Sequential):
     A head with several 3x3 conv layers (each followed by norm & relu) and then
     several fc layers (each followed by relu).
     """
-
-    __jit_unused_properties__ = ["output_shape"]
 
     @configurable
     def __init__(
@@ -98,6 +97,7 @@ class FastRCNNConvFCHead(nn.Sequential):
         return x
 
     @property
+    @torch.jit.unused
     def output_shape(self):
         """
         Returns:
