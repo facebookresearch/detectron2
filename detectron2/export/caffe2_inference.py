7aae6bbd5156794964ee98d19ad28eb07e679ff9
Yuxin Wu <yuxinwu@fb.com>
2020-10-30 15:09:36 -0700

    move device inference into ProtobufModel
    Reviewed By: newstzpz
    
    Differential Revision: D24378505
    
    fbshipit-source-id: a8112e774b4bf65e7e04c010a37f5c3600e8efb5

--- detectron2/export/caffe2_inference.py
+++ detectron2/export/caffe2_inference.py
@@ -3,6 +3,7 @@
 import collections
 import logging
 import numpy as np
+from itertools import count
 import torch
 from caffe2.proto import caffe2_pb2
 from caffe2.python import core
@@ -13,35 +14,77 @@ from .shared import ScopedWS, get_pb_arg_vali, get_pb_arg_vals, infer_device_typ
 logger = logging.getLogger(__name__)
 
 
+# ===== ref: mobile-vision's 'Caffe2Wrapper' class ======
 class ProtobufModel(torch.nn.Module):
     """
-    A class works just like nn.Module in terms of inference, but running
-    caffe2 model under the hood. Input/Output are Dict[str, tensor] whose keys
-    are in external_input/output.
+    Wrapper of a caffe2's protobuf model.
+    It works just like nn.Module, but running caffe2 under the hood.
+    Input/Output are Dict[str, tensor] whose keys are in external_input/output.
     """
 
+    _ids = count(0)
+
     def __init__(self, predict_net, init_net):
-        logger.info("Initializing ProtobufModel ...")
+        logger.info(f"Initializing ProtobufModel for: {predict_net.name} ...")
         super().__init__()
         assert isinstance(predict_net, caffe2_pb2.NetDef)
         assert isinstance(init_net, caffe2_pb2.NetDef)
-        self.ws_name = "__ws_tmp__"
+        # create unique temporary workspace for each instance
+        self.ws_name = "__tmp_ProtobufModel_{}__".format(next(self._ids))
         self.net = core.Net(predict_net)
 
+        logger.info("Running init_net once to fill the parameters ...")
         with ScopedWS(self.ws_name, is_reset=True, is_cleanup=False) as ws:
             ws.RunNetOnce(init_net)
+            uninitialized_external_input = []
             for blob in self.net.Proto().external_input:
                 if blob not in ws.Blobs():
+                    uninitialized_external_input.append(blob)
                     ws.CreateBlob(blob)
             ws.CreateNet(self.net)
 
         self._error_msgs = set()
+        self._input_blobs = uninitialized_external_input
+
+    def _infer_output_devices(self, inputs_dict):
+        """
+        Returns:
+            list[str]: list of device for each external output
+        """
+
+        def _get_device_type(torch_tensor):
+            assert torch_tensor.device.type in ["cpu", "cuda"]
+            assert torch_tensor.device.index == 0
+            return torch_tensor.device.type
+
+        predict_net = self.net.Proto()
+        input_device_types = {
+            (name, 0): _get_device_type(tensor) for name, tensor in inputs_dict.items()
+        }
+        device_type_map = infer_device_type(
+            predict_net, known_status=input_device_types, device_name_style="pytorch"
+        )
+        ssa, versions = core.get_ssa(predict_net)
+        versioned_outputs = [(name, versions[name]) for name in predict_net.external_output]
+        output_devices = [device_type_map[outp] for outp in versioned_outputs]
+        return output_devices
 
     def forward(self, inputs_dict):
-        assert all(inp in self.net.Proto().external_input for inp in inputs_dict)
+        """
+        Args:
+            inputs_dict (dict[str->torch.Tensor])
+
+        Returns:
+            dict[str->torch.Tensor]
+        """
+        assert set(inputs_dict.keys()) == set(
+            self._input_blobs
+        ), f"Input dict must match the names {self._input_blobs}! Got f{inputs_dict.keys()}."
+
         with ScopedWS(self.ws_name, is_reset=False, is_cleanup=False) as ws:
             for b, tensor in inputs_dict.items():
                 ws.FeedBlob(b, tensor)
+
             try:
                 ws.RunNet(self.net.Proto().name)
             except RuntimeError as e:
@@ -60,8 +103,22 @@ class ProtobufModel(torch.nn.Module):
                 # Needs to create uninitialized blob to make the net runable.
                 # This is "equivalent" to: ws.RemoveBlob(b) then ws.CreateBlob(b),
                 # but there'no such API.
-                ws.FeedBlob(b, "{}, a C++ native class of type nullptr (uninitialized).".format(b))
+                ws.FeedBlob(b, f"{b}, a C++ native class of type nullptr (uninitialized).")
 
+        # Cast output to torch.Tensor on the desired device
+        output_devices = (
+            self._infer_output_devices(inputs_dict)
+            if any(t.device.type != "cpu" for t in inputs_dict.values())
+            else ["cpu" for _ in self.net.Proto().external_output]
+        )
+
+        for name, device in zip(self.net.Proto().external_output, output_devices):
+            c2_output = outputs_dict[name]
+            if not isinstance(c2_output, np.ndarray):
+                raise RuntimeError(
+                    "Invalid output for blob {}, received: {}".format(name, c2_output)
+                )
+            outputs_dict[name] = torch.Tensor(c2_output).to(device=device)
         return outputs_dict
 
 
@@ -91,24 +148,6 @@ class ProtobufDetectionModel(torch.nn.Module):
         else:
             self._convert_outputs = convert_outputs
 
-    def _infer_output_devices(self, inputs_dict):
-        def _get_device_type(torch_tensor):
-            assert torch_tensor.device.type in ["cpu", "cuda"]
-            assert torch_tensor.device.index == 0
-            return torch_tensor.device.type
-
-        predict_net = self.protobuf_model.net.Proto()
-        input_device_types = {
-            (name, 0): _get_device_type(tensor) for name, tensor in inputs_dict.items()
-        }
-        device_type_map = infer_device_type(
-            predict_net, known_status=input_device_types, device_name_style="pytorch"
-        )
-        ssa, versions = core.get_ssa(predict_net)
-        versioned_outputs = [(name, versions[name]) for name in predict_net.external_output]
-        output_devices = [device_type_map[outp] for outp in versioned_outputs]
-        return output_devices
-
     def _convert_inputs(self, batched_inputs):
         # currently all models convert inputs in the same way
         data, im_info = convert_batched_inputs_to_c2_format(
@@ -119,18 +158,4 @@ class ProtobufDetectionModel(torch.nn.Module):
     def forward(self, batched_inputs):
         c2_inputs = self._convert_inputs(batched_inputs)
         c2_results = self.protobuf_model(c2_inputs)
-
-        if any(t.device.type != "cpu" for _, t in c2_inputs.items()):
-            output_devices = self._infer_output_devices(c2_inputs)
-        else:
-            output_devices = ["cpu" for _ in self.protobuf_model.net.Proto().external_output]
-
-        def _cast_caffe2_blob_to_torch_tensor(blob, device):
-            return torch.Tensor(blob).to(device) if isinstance(blob, np.ndarray) else None
-
-        c2_results = {
-            name: _cast_caffe2_blob_to_torch_tensor(c2_results[name], device)
-            for name, device in zip(self.protobuf_model.net.Proto().external_output, output_devices)
-        }
-
         return self._convert_outputs(batched_inputs, c2_inputs, c2_results)
