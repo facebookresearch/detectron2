fc4aa4fb299685a91c17519e8b77e0438d573123
Yuxin Wu <yuxinwu@fb.com>
2020-11-07 19:47:05 -0800

    avoid incontiguous image read
    Summary: fix https://github.com/facebookresearch/detectron2/issues/2236
    
    Reviewed By: alexander-kirillov
    
    Differential Revision: D24802387
    
    fbshipit-source-id: a561c86e56ce660e4f4370c1aee36beb11a8fa9b

--- detectron2/modeling/test_time_augmentation.py
+++ detectron2/modeling/test_time_augmentation.py
@@ -178,7 +178,7 @@ class GeneralizedRCNNWithTTA(nn.Module):
             ret = copy.copy(dataset_dict)
             if "image" not in ret:
                 image = read_image(ret.pop("file_name"), self.tta_mapper.image_format)
-                image = torch.from_numpy(image).permute(2, 0, 1)  # CHW
+                image = torch.from_numpy(np.ascontiguousarray(image.transpose(2, 0, 1)))  # CHW
                 ret["image"] = image
             if "height" not in ret and "width" not in ret:
                 ret["height"] = image.shape[1]
