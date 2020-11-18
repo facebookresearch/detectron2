fix gradient clipping
Summary: Pull Request resolved: https://github.com/fairinternal/detectron2/pull/485

Reviewed By: alexander-kirillov

Differential Revision: D25062744

Pulled By: ppwwyyxx

fbshipit-source-id: b6df021ca10c36a15e408771b37589ed9fb068c6

projects/Panoptic-DeepLab/train_net.py
@@ -112,14 +112,14 @@ class Trainer(DefaultTrainer):
 
         optimizer_type = cfg.SOLVER.OPTIMIZER
         if optimizer_type == "SGD":
-            return maybe_add_gradient_clipping(torch.optim.SGD)(
+            return maybe_add_gradient_clipping(cfg, torch.optim.SGD)(
                 params,
                 cfg.SOLVER.BASE_LR,
                 momentum=cfg.SOLVER.MOMENTUM,
                 nesterov=cfg.SOLVER.NESTEROV,
             )
         elif optimizer_type == "ADAM":
-            return maybe_add_gradient_clipping(torch.optim.Adam)(params, cfg.SOLVER.BASE_LR)
+            return maybe_add_gradient_clipping(cfg, torch.optim.Adam)(params, cfg.SOLVER.BASE_LR)
         else:
             raise NotImplementedError(f"no optimizer type {optimizer_type}")
 