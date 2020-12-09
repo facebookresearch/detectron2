bba2f712dc056ce3bbc1c246c11299194d92f257
Vasil Khalidov <vkhalidov@fb.com>
2020-12-08 17:05:50 -0800

    extract mask loss for reuse
    Summary: Mask loss is extracted into a separate module to be reused in different losses.
    
    Reviewed By: patricklabatut
    
    Differential Revision: D24132792
    
    fbshipit-source-id: 3983ef31123fd1f2d5b0aaa3656e29c5ea8b6c44

--- projects/DensePose/densepose/modeling/losses/chart.py
+++ projects/DensePose/densepose/modeling/losses/chart.py
@@ -1,75 +1,15 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
 
-from dataclasses import dataclass
-from typing import Any, Iterable, List, Optional
+from typing import Any, List
 import torch
 from torch.nn import functional as F
 
 from detectron2.config import CfgNode
 from detectron2.structures import Instances
 
+from .mask import MaskLoss
 from .registry import DENSEPOSE_LOSS_REGISTRY
 from .utils import BilinearInterpolationHelper, LossDict, SingleTensorsHelper, resample_data
-
-
-@dataclass
-class DataForMaskLoss:
-    """
-    Contains mask GT and estimated data for proposals from multiple images:
-    """
-
-    # tensor of size (K, H, W) containing GT labels
-    masks_gt: Optional[torch.Tensor] = None
-    # tensor of size (K, C, H, W) containing estimated scores
-    masks_est: Optional[torch.Tensor] = None
-
-
-def extract_data_for_mask_loss_from_matches(
-    proposals_targets: Iterable[Instances], estimated_segm: torch.Tensor
-) -> DataForMaskLoss:
-    """
-    Extract data for mask loss from instances that contain matched GT and
-    estimated bounding boxes.
-    Args:
-        proposals_targets: Iterable[Instances]
-            matched GT and estimated results, each item in the iterable
-            corresponds to data in 1 image
-        estimated_segm: tensor(K, C, S, S) of float - raw unnormalized
-            segmentation scores, here S is the size to which GT masks are
-            to be resized
-    Return:
-        masks_est: tensor(K, C, S, S) of float - class scores
-        masks_gt: tensor(K, S, S) of int64 - labels
-    """
-    data = DataForMaskLoss()
-    masks_gt = []
-    offset = 0
-    assert estimated_segm.shape[2] == estimated_segm.shape[3], (
-        f"Expected estimated segmentation to have a square shape, "
-        f"but the actual shape is {estimated_segm.shape[2:]}"
-    )
-    mask_size = estimated_segm.shape[2]
-    num_proposals = sum(inst.proposal_boxes.tensor.size(0) for inst in proposals_targets)
-    num_estimated = estimated_segm.shape[0]
-    assert (
-        num_proposals == num_estimated
-    ), "The number of proposals {} must be equal to the number of estimates {}".format(
-        num_proposals, num_estimated
-    )
-
-    for proposals_targets_per_image in proposals_targets:
-        n_i = proposals_targets_per_image.proposal_boxes.tensor.size(0)
-        if not n_i:
-            continue
-        gt_masks_per_image = proposals_targets_per_image.gt_masks.crop_and_resize(
-            proposals_targets_per_image.proposal_boxes.tensor, mask_size
-        ).to(device=estimated_segm.device)
-        masks_gt.append(gt_masks_per_image)
-        offset += n_i
-    if masks_gt:
-        data.masks_est = estimated_segm
-        data.masks_gt = torch.cat(masks_gt, dim=0)
-    return data
 
 
 @DENSEPOSE_LOSS_REGISTRY.register()
@@ -114,6 +54,7 @@ class DensePoseChartLoss:
         self.n_segm_chan  = cfg.MODEL.ROI_DENSEPOSE_HEAD.NUM_COARSE_SEGM_CHANNELS
         # fmt: on
         self.segm_trained_by_masks = cfg.MODEL.ROI_DENSEPOSE_HEAD.COARSE_SEGM_TRAINED_BY_MASKS
+        self.mask_loss = MaskLoss()
 
     def __call__(
         self, proposals_with_gt: List[Instances], densepose_predictor_outputs: Any
@@ -146,63 +87,12 @@ class DensePoseChartLoss:
             losses_densepose = self.produce_densepose_losses(
                 proposals_with_gt, densepose_predictor_outputs
             )
-            losses_mask = self.produce_mask_losses(proposals_with_gt, densepose_predictor_outputs)
-            return {**losses_densepose, **losses_mask}
-
-    def produce_fake_mask_losses(self, densepose_predictor_outputs: Any) -> LossDict:
-        """
-        Fake coarse segmentation loss used when no suitable ground truth data
-        was found in a batch. The loss has a value 0 and is primarily used to
-        construct the computation graph, so that `DistributedDataParallel`
-        has similar graphs on all GPUs and can perform reduction properly.
-
-        Args:
-            densepose_predictor_outputs: DensePose predictor outputs, an object
-                of a dataclass that is assumed to have `coarse_segm`
-                attribute
-        Return:
-            dict: str -> tensor: dict of losses with a single entry
-                `loss_densepose_S` with 0 value
-        """
-        return {"loss_densepose_S": densepose_predictor_outputs.coarse_segm.sum() * 0}
-
-    def produce_mask_losses(
-        self, proposals_with_gt: List[Instances], densepose_predictor_outputs: Any
-    ) -> LossDict:
-        """
-        Computes coarse segmentation loss as cross-entropy for raw unnormalized
-        scores given ground truth labels.
-
-        Args:
-            proposals_with_gt (list of Instances): detections with associated ground truth data
-            densepose_predictor_outputs: an object of a dataclass that contains predictor outputs
-                with estimated values; assumed to have the following attribute:
-                * coarse_segm (tensor of shape [N, D, S, S]): coarse segmentation estimates
-                    as raw unnormalized scores
-            where N is the number of detections, S is the estimate size ( = width = height) and
-            D is the number of coarse segmentation channels.
-        Return:
-            dict: str -> tensor: dict of losses with a single entry:
-            * `loss_densepose_S`: cross entropy for raw unnormalized scores for coarse
-                segmentation given ground truth labels
-        """
-        if not len(proposals_with_gt):
-            return self.produce_fake_mask_losses(densepose_predictor_outputs)
-        # densepose outputs are computed for all images and all bounding boxes;
-        # i.e. if a batch has 4 images with (3, 1, 2, 1) proposals respectively,
-        # the outputs will have size(0) == 3+1+2+1 == 7
-        with torch.no_grad():
-            mask_loss_data = extract_data_for_mask_loss_from_matches(
-                proposals_with_gt, densepose_predictor_outputs.coarse_segm
-            )
-        if (mask_loss_data.masks_gt is None) or (mask_loss_data.masks_est is None):
-            return self.produce_fake_mask_losses(densepose_predictor_outputs)
-        return {
-            "loss_densepose_S": F.cross_entropy(
-                mask_loss_data.masks_est, mask_loss_data.masks_gt.long()
-            )
-            * self.w_segm
-        }
+            return {
+                "loss_densepose_S": (
+                    self.w_segm * self.mask_loss(proposals_with_gt, densepose_predictor_outputs)
+                ),
+                **losses_densepose,
+            }
 
     def produce_fake_densepose_losses(self, densepose_predictor_outputs: Any) -> LossDict:
         """
