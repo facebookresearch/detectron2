e697fa492a8391171cd73c8bdf74473330103b0a
Zhipeng Han <hanzhipeng9@gmail.com>
2020-12-11 09:38:53 +0000

    Format
    Signed-off-by: Zhipeng Han <hanzhipeng9@gmail.com>

--- detectron2/config/config.py
+++ detectron2/config/config.py
@@ -221,9 +221,7 @@ def _get_args_from_config(from_config_func, *args, **kwargs):
             name = from_config_func.__name__
         else:
             name = f"{from_config_func.__self__}.from_config"
-        raise TypeError(
-            f"{name} must take 'cfg' as the first argument!"
-        )
+        raise TypeError(f"{name} must take 'cfg' as the first argument!")
     support_var_arg = any(
         param.kind in [param.VAR_POSITIONAL, param.VAR_KEYWORD]
         for param in signature.parameters.values()
