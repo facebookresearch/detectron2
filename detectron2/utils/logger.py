5c8b251d24e5d84e556859c98d0e30b970c78ba8
Yuxin Wu <yuxinwu@fb.com>
2021-03-03 10:57:03 -0800

    use iopath to handle append error
    Summary: https://fb.workplace.com/groups/939200583171018/permalink/1098163433941398
    
    Reviewed By: theschnitz
    
    Differential Revision: D26731873
    
    fbshipit-source-id: 749984dc6bd0ea9f49b3a08be59d04611ef3d288

--- detectron2/utils/logger.py
+++ detectron2/utils/logger.py
@@ -100,7 +100,8 @@ def setup_logger(
 # with the same file name can safely write to the same file.
 @functools.lru_cache(maxsize=None)
 def _cached_log_stream(filename):
-    io = PathManager.open(filename, "a")
+    # use 1K buffer if writing to cloud storage
+    io = PathManager.open(filename, "a", buffering=1024 if "://" in filename else -1)
     atexit.register(io.close)
     return io
 
