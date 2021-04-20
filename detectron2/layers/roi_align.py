7d17439d7d0f3439273dde1e190eea6c005fbc08
Jerry Zhang <jerryzh@fb.com>
2021-04-20 10:31:00 -0700

    Remove redundant dequant/quant between backbone and proposal generator and roi heads
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/detectron2/pull/2932
    
    Pull Request resolved: https://github.com/fairinternal/detectron2/pull/533
    
    Actually only the one between backbone and proposal generator is redundant unless
    we implement a quantized version for roi_align (currently roi align is expecting
    a float32 input, so we have to dequantize it in the code)
    
    Reviewed By: vkuzo, ppwwyyxx
    
    Differential Revision: D27837648
    
    fbshipit-source-id: 74fd87b31d6f701dd2917e90efad558e9d5c5b21

--- detectron2/layers/roi_align.py
+++ detectron2/layers/roi_align.py
@@ -53,6 +53,8 @@ class ROIAlign(nn.Module):
             rois: Bx5 boxes. First column is the index into N. The other 4 columns are xyxy.
         """
         assert rois.dim() == 2 and rois.size(1) == 5
+        if input.is_quantized:
+            input = input.dequantize()
         return roi_align(
             input,
             rois.to(dtype=input.dtype),
