94af461322feba0c3cadde886367445d62bc45a7
AlanDecode <zytbuaa1415@gmail.com>
2021-04-23 14:43:44 +0800

    fix deprecation warning with torch.nonzero

--- detectron2/modeling/roi_heads/fast_rcnn.py
+++ detectron2/modeling/roi_heads/fast_rcnn.py
@@ -151,7 +151,7 @@ def fast_rcnn_inference_single_image(
     filter_mask = scores > score_thresh  # R x K
     # R' x 2. First column contains indices of the R predictions;
     # Second column contains indices of classes.
-    filter_inds = filter_mask.nonzero()
+    filter_inds = torch.nonzero(filter_mask, as_tuple=False)
     if num_bbox_reg_classes == 1:
         boxes = boxes[filter_inds[:, 0], 0]
     else:
