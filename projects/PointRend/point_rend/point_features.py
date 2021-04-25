c1689c7d7b1c1af0157cd90b84f296b7b2ce2124
Bowen Cheng <bowenc0221@gmail.com>
2021-04-25 00:42:01 -0700

    refactor PointRend mask head
    Summary: Pull Request resolved: https://github.com/facebookresearch/detectron2/pull/2875
    
    Test Plan:
    test old model:
     buck run mode/opt //vision/fair/detectron2/projects/PointRend:train_net  -- --num-gpus 8
     --dist-url auto --eval-only --config-file
     configs/InstanceSegmentation/pointrend_rcnn_R_50_FPN_3x_coco.yaml MODEL.DEVICE cuda
     DATALOADER.NUM_WORKERS 0  MODEL.WEIGHTS
     manifold://fair_vision_data/tree/detectron2/model_zoo/PointRend/InstanceSegmentation/pointrend_rcnn_R_50_FPN_1x_coco/164254221/model_final_88c6f8.pkl
     DATASETS.TEST '("coco_2017_val_100",)'  MODEL.ROI_HEADS.NAME PointRendROIHeads
     MODEL.ROI_MASK_HEAD.NAME CoarseMaskHead
    
    Reviewed By: alexander-kirillov
    
    Differential Revision: D27574366
    
    Pulled By: ppwwyyxx
    
    fbshipit-source-id: ebfd0b16dca71257b4f793cb151433ab3fde9360

--- projects/PointRend/point_rend/point_features.py
+++ projects/PointRend/point_rend/point_features.py
@@ -3,7 +3,7 @@ import torch
 from torch.nn import functional as F
 
 from detectron2.layers import cat
-from detectron2.structures import Boxes
+from detectron2.structures import BitMasks, Boxes
 
 
 """
@@ -223,3 +223,46 @@ def get_point_coords_wrt_image(boxes_coords, point_coords):
         point_coords_wrt_image[:, :, 0] += boxes_coords[:, None, 0]
         point_coords_wrt_image[:, :, 1] += boxes_coords[:, None, 1]
     return point_coords_wrt_image
+
+
+def sample_point_labels(instances, point_coords):
+    """
+    Sample point labels from ground truth mask given point_coords.
+
+    Args:
+        instances (list[Instances]): A list of N Instances, where N is the number of images
+            in the batch. So, i_th elememt of the list contains R_i objects and R_1 + ... + R_N is
+            equal to R. The ground-truth gt_masks in each instance will be used to compute labels.
+        points_coords (Tensor): A tensor of shape (R, P, 2), where R is the total number of
+            instances and P is the number of points for each instance. The coordinates are in
+            the absolute image pixel coordinate space, i.e. [0, H] x [0, W].
+
+    Returns:
+        Tensor: A tensor of shape (R, P) that contains the labels of P sampled points.
+    """
+    with torch.no_grad():
+        gt_mask_logits = []
+        point_coords_splits = torch.split(
+            point_coords, [len(instances_per_image) for instances_per_image in instances]
+        )
+        for i, instances_per_image in enumerate(instances):
+            if len(instances_per_image) == 0:
+                continue
+            assert isinstance(
+                instances_per_image.gt_masks, BitMasks
+            ), "Point head works with GT in 'bitmask' format. Set INPUT.MASK_FORMAT to 'bitmask'."
+
+            gt_bit_masks = instances_per_image.gt_masks.tensor
+            h, w = instances_per_image.gt_masks.image_size
+            scale = torch.tensor([w, h], dtype=torch.float, device=gt_bit_masks.device)
+            points_coord_grid_sample_format = point_coords_splits[i] / scale
+            gt_mask_logits.append(
+                point_sample(
+                    gt_bit_masks.to(torch.float32).unsqueeze(1),
+                    points_coord_grid_sample_format,
+                    align_corners=False,
+                ).squeeze(1)
+            )
+
+    point_labels = cat(gt_mask_logits)
+    return point_labels
