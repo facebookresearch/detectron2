20226d177ef66734d1e2180cfa98f7be784e4376
QingQiz <sofeeys@outlook.com>
2021-05-07 13:02:21 -0700

    support custom attributes in proposals predicted by custom RPN
    Summary:
    fix: throw KeyError when there are custom attributes in proposals
    
    Pull Request resolved: https://github.com/facebookresearch/detectron2/pull/2964
    
    Reviewed By: theschnitz
    
    Differential Revision: D28274073
    
    Pulled By: ppwwyyxx
    
    fbshipit-source-id: 97c52a6ce2378c1fe91c55fd5aad5a601dbcd937

--- detectron2/modeling/proposal_generator/proposal_utils.py
+++ detectron2/modeling/proposal_generator/proposal_utils.py
@@ -1,7 +1,7 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
 import logging
 import math
-from typing import List, Tuple
+from typing import List, Tuple, Union
 import torch
 
 from detectron2.layers import batched_nms, cat
@@ -130,13 +130,15 @@ def find_top_rpn_proposals(
     return results
 
 
-def add_ground_truth_to_proposals(gt_boxes, proposals):
+def add_ground_truth_to_proposals(
+    gt: Union[List[Instances], List[Boxes]], proposals: List[Instances]
+) -> List[Instances]:
     """
     Call `add_ground_truth_to_proposals_single_image` for all images.
 
     Args:
-        gt_boxes(list[Boxes]): list of N elements. Element i is a Boxes
-            representing the gound-truth for image i.
+        gt(Union[List[Instances], List[Boxes]): list of N elements. Element i is a Instances
+            representing the ground-truth for image i.
         proposals (list[Instances]): list of N elements. Element i is a Instances
             representing the proposals for image i.
 
@@ -144,29 +146,37 @@ def add_ground_truth_to_proposals(gt_boxes, proposals):
         list[Instances]: list of N Instances. Each is the proposals for the image,
             with field "proposal_boxes" and "objectness_logits".
     """
-    assert gt_boxes is not None
+    assert gt is not None
 
-    assert len(proposals) == len(gt_boxes)
+    if len(proposals) != len(gt):
+        raise ValueError("proposals and gt should have the same length as the number of images!")
     if len(proposals) == 0:
         return proposals
 
     return [
-        add_ground_truth_to_proposals_single_image(gt_boxes_i, proposals_i)
-        for gt_boxes_i, proposals_i in zip(gt_boxes, proposals)
+        add_ground_truth_to_proposals_single_image(gt_i, proposals_i)
+        for gt_i, proposals_i in zip(gt, proposals)
     ]
 
 
-def add_ground_truth_to_proposals_single_image(gt_boxes, proposals):
+def add_ground_truth_to_proposals_single_image(
+    gt: Union[Instances, Boxes], proposals: Instances
+) -> Instances:
     """
-    Augment `proposals` with ground-truth boxes from `gt_boxes`.
+    Augment `proposals` with `gt`.
 
     Args:
-        Same as `add_ground_truth_to_proposals`, but with gt_boxes and proposals
+        Same as `add_ground_truth_to_proposals`, but with gt and proposals
         per image.
 
     Returns:
         Same as `add_ground_truth_to_proposals`, but for only one image.
     """
+    if isinstance(gt, Boxes):
+        # convert Boxes to Instances
+        gt = Instances(proposals.image_size, gt_boxes=gt)
+
+    gt_boxes = gt.gt_boxes
     device = proposals.objectness_logits.device
     # Assign all ground-truth boxes an objectness logit corresponding to
     # P(object) = sigmoid(logit) =~ 1.
@@ -174,9 +184,17 @@ def add_ground_truth_to_proposals_single_image(gt_boxes, proposals):
     gt_logits = gt_logit_value * torch.ones(len(gt_boxes), device=device)
 
     # Concatenating gt_boxes with proposals requires them to have the same fields
-    gt_proposal = Instances(proposals.image_size)
+    gt_proposal = Instances(proposals.image_size, **gt.get_fields())
     gt_proposal.proposal_boxes = gt_boxes
     gt_proposal.objectness_logits = gt_logits
+
+    for key in proposals.get_fields().keys():
+        assert gt_proposal.has(
+            key
+        ), "The attribute '{}' in `proposals` does not exist in `gt`".format(key)
+
+    # NOTE: Instances.cat only use fields from the first item. Extra fields in latter items
+    # will be thrown away.
     new_proposals = Instances.cat([proposals, gt_proposal])
 
     return new_proposals
