c27c3d30a8947f8585bf07bcf2de140964237b33
levan92 <lingevan0208@gmail.com>
2021-07-08 11:28:16 +0800

    remove append iter; save without iter in filename

--- detectron2/engine/hooks.py
+++ detectron2/engine/hooks.py
@@ -214,7 +214,6 @@ class BestCheckpointer(HookBase):
         checkpointer: Checkpointer,
         val_metric: str = "bbox/AP50",
         file_prefix: str = "model_best",
-        append_iter: bool = False,
     ) -> None:
         """
         Args:
@@ -222,7 +221,6 @@ class BestCheckpointer(HookBase):
             checkpointer: the checkpointer object used to save checkpoints.
             val_metric (str): chosen validation metric to track for best checkpointing
             file_prefix (str): the prefix of checkpoint's filename, defaults to "model_best"
-            append_iter (bool): if True, checkpoint file name will have iteration num appended, else write to the same checkpoint file.
         """
         self._logger = logging.getLogger(__name__)
         self._period = eval_period
@@ -231,18 +229,12 @@ class BestCheckpointer(HookBase):
         self._file_prefix = file_prefix
         self.best_metric = None
         self.best_iter = None
-        self._append_iter = append_iter
 
     def _best_checking(self):
         latest_metric, metric_iter = self.trainer.storage.latest().get(self._val_metric)
         if self.best_metric is None or latest_metric > self.best_metric:
-            if self._append_iter:
-                additional_state = {"iteration": metric_iter}
-                save_name = f"{self._file_prefix}_{metric_iter:07d}"
-            else:
-                save_name = f"{self._file_prefix}"
-                additional_state = {}
-            self._checkpointer.save(save_name, **additional_state)
+            additional_state = {"iteration": metric_iter}
+            self._checkpointer.save(f"{self._file_prefix}", **additional_state)
             if self.best_metric is None:
                 self._logger.info(
                     f"Saved first model with latest eval score for {self._val_metric} at {latest_metric:0.5f}"
