9a3e1b55987ffb82190b6776d35e69c45e2b0101
Peter Bull <pjbull@gmail.com>
2021-08-02 10:26:47 -0400

    minimal setup.py

---
+++ projects/DensePose/setup.py
@@ -0,0 +1,29 @@
+
+from setuptools import setup, find_packages
+
+try:
+    import torch
+except ImportError:
+    raise Exception("""
+You must install PyTorch prior to installing DensePose:
+pip install torch
+
+For more information:
+    https://pytorch.org/get-started/locally/
+    """)
+
+setup(
+    name='DensePose',
+    author='FAIR',
+    url="https://github.com/facebookresearch/detectron2/tree/master/projects/DensePose",
+    packages=find_packages(),
+    python_requires=">=3.6",
+    install_requires=[
+        'av >= 8.0.3',
+        'detectron2@git+https://github.com/facebookresearch/detectron2.git',
+        'opencv-python-headless >= 4.5.3.56',
+        'scipy >= 1.5.4',
+        'torch >= 1.9.0',
+        'torchvision >= 0.10.0',
+    ],
+)
