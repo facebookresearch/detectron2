50a55919e0bca322e6a5e49ffeb8275ff8a6010a
Peter Bull <pjbull@gmail.com>
2021-08-02 10:40:05 -0400

    lint and format

--- projects/DensePose/setup.py
+++ projects/DensePose/setup.py
@@ -1,29 +1,30 @@
-
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 try:
-    import torch
+    import torch  # noqa: F401
 except ImportError:
-    raise Exception("""
+    raise Exception(
+        """
 You must install PyTorch prior to installing DensePose:
 pip install torch
 
 For more information:
     https://pytorch.org/get-started/locally/
-    """)
+    """
+    )
 
 setup(
-    name='DensePose',
-    author='FAIR',
+    name="DensePose",
+    author="FAIR",
     url="https://github.com/facebookresearch/detectron2/tree/master/projects/DensePose",
     packages=find_packages(),
     python_requires=">=3.6",
     install_requires=[
-        'av >= 8.0.3',
-        'detectron2@git+https://github.com/facebookresearch/detectron2.git',
-        'opencv-python-headless >= 4.5.3.56',
-        'scipy >= 1.5.4',
-        'torch >= 1.9.0',
-        'torchvision >= 0.10.0',
+        "av >= 8.0.3",
+        "detectron2@git+https://github.com/facebookresearch/detectron2.git",
+        "opencv-python-headless >= 4.5.3.56",
+        "scipy >= 1.5.4",
+        "torch >= 1.9.0",
+        "torchvision >= 0.10.0",
     ],
 )
