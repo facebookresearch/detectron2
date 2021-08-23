f327aac84dbfe143a627b67f31950d5d4472d16a
Yuxin Wu <yuxinwu@fb.com>
2021-08-23 10:18:42 -0700

    let MapDataset support iterable
    Summary:
    Most production use cases needs error handling.
    This is not available in pytorch's `.map()`.
    
    This is useful as we move towards using more IterableDataset
    
    Differential Revision: D30123656
    
    fbshipit-source-id: 17c4caa3e4ed18c1b073bbfe8309ca3b3681a59b

--- tests/data/test_dataset.py
+++ tests/data/test_dataset.py
@@ -1,12 +1,14 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
 
 import os
+import pickle
 import sys
 import unittest
 from functools import partial
+import torch
 from iopath.common.file_io import LazyPath
 
-from detectron2.data.build import DatasetFromList
+from detectron2.data.build import DatasetFromList, MapDataset
 
 
 def _a_slow_func(x):
@@ -26,3 +28,36 @@ class TestDatasetFromList(unittest.TestCase):
             path = dataset[i]["file_name"]
             self.assertTrue(isinstance(path, LazyPath))
             self.assertEqual(os.fspath(path), _a_slow_func(i))
+
+
+class TestMapDataset(unittest.TestCase):
+    @staticmethod
+    def map_func(x):
+        if x == 2:
+            return None
+        return x * 2
+
+    def test_map_style(self):
+        ds = DatasetFromList([1, 2, 3])
+        ds = MapDataset(ds, TestMapDataset.map_func)
+        self.assertEqual(ds[0], 2)
+        self.assertEqual(ds[2], 6)
+        self.assertIn(ds[1], [2, 6])
+
+    def test_iter_style(self):
+        class DS(torch.utils.data.IterableDataset):
+            def __iter__(self):
+                yield from [1, 2, 3]
+
+        ds = DS()
+        ds = MapDataset(ds, TestMapDataset.map_func)
+        self.assertIsInstance(ds, torch.utils.data.IterableDataset)
+
+        data = list(iter(ds))
+        self.assertEqual(data, [2, 6])
+
+    def test_pickleability(self):
+        ds = DatasetFromList([1, 2, 3])
+        ds = MapDataset(ds, lambda x: x * 2)
+        ds = pickle.loads(pickle.dumps(ds))
+        self.assertEqual(ds[0], 2)
