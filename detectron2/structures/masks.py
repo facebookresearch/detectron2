2a1cec4c647dbe8a05f2d5b599d7d4c7cf9f04ad
Bowen Cheng <bowenc0221@gmail.com>
2021-09-25 00:11:30 -0700

    support using bitmask with LSJ
    Summary:
    `polygons_to_bitmask` fails with empty polygon which could happen with LSJ.
    
    Pull Request resolved: https://github.com/fairinternal/detectron2/pull/564
    
    Reviewed By: HannaMao
    
    Differential Revision: D31181422
    
    Pulled By: ppwwyyxx
    
    fbshipit-source-id: 5f955e04518bae153401df0760840f5daf0da66a

--- detectron2/structures/masks.py
+++ detectron2/structures/masks.py
@@ -28,7 +28,9 @@ def polygons_to_bitmask(polygons: List[np.ndarray], height: int, width: int) -> 
     Returns:
         ndarray: a bool mask of shape (height, width)
     """
-    assert len(polygons) > 0, "COCOAPI does not support empty polygons"
+    if len(polygons) == 0:
+        # COCOAPI does not support empty polygons
+        return np.zeros((height, width)).astype(np.bool)
     rles = mask_util.frPyObjects(polygons, height, width)
     rle = mask_util.merge(rles)
     return mask_util.decode(rle).astype(np.bool)
