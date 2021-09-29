188916875a00fb01ec4fad0fa4edda5f1eaca039
prashanth <prashanth.flash98@gmail.com>
2021-09-29 13:59:51 -0400

    config name chanegs

--- detectron2/engine/defaults.py
+++ detectron2/engine/defaults.py
@@ -474,7 +474,7 @@ class DefaultTrainer(TrainerBase):
         Returns:
             list[EventWriter]: a list of :class:`EventWriter` objects.
         """
-        return default_writers(self.cfg.OUTPUT_DIR, self.max_iter, self.cfg.WANDB.PROJECT_NAME)
+        return default_writers(self.cfg.OUTPUT_DIR, self.max_iter, "detectron2")
 
     def train(self):
         """
