73c7282928df4b76d53a2ab134994068d2fc7359
Alex Emelyanov <alex84_84_84@mail.ru>
2021-11-15 18:36:30 +0300

    run custom train

--- supervisely/sl_to_detectron.py
+++ supervisely/sl_to_detectron.py
@@ -26,7 +26,7 @@ for class_index, obj_class in enumerate(meta.obj_classes):
     all_classes[obj_class.name] = class_index
 
 
-def get_sl_dicts(project_id):
+def get_sl_dicts():
 
     project_info = api.project.get_info_by_id(project_id)
     meta_json = api.project.get_meta(project_info.id)
@@ -60,6 +60,7 @@ def get_sl_dicts(project_id):
                     obj = {
                         "bbox": [rect.left, rect.top, rect.right, rect.bottom],
                         "bbox_mode": BoxMode.XYXY_ABS,
+                        "segmentation": [],
                         "category_id": all_classes[label.obj_class.name],
                     }
 
@@ -76,13 +77,37 @@ MetadataCatalog.get("lemons").thing_classes = list(all_classes.keys())
 
 balloon_metadata = MetadataCatalog.get("lemons")
 
-dataset_dicts = get_sl_dicts(project_id)
+dataset_dicts = get_sl_dicts()
 
-for d in random.sample(dataset_dicts, 3):
-    img = cv2.imread(d["file_name"])
-    visualizer = Visualizer(img[:, :, ::-1], metadata=balloon_metadata, scale=0.5)
-    out = visualizer.draw_dataset_dict(d)
-    cv2.imshow('', out.get_image()[:, :, ::-1])
-    if cv2.waitKey(0) == 27:
-        break
+# for d in random.sample(dataset_dicts, 3):
+#     img = cv2.imread(d["file_name"])
+#     visualizer = Visualizer(img[:, :, ::-1], metadata=balloon_metadata, scale=0.5)
+#     out = visualizer.draw_dataset_dict(d)
+#     cv2.imshow('', out.get_image()[:, :, ::-1])
+#     if cv2.waitKey(0) == 27:
+#         break
 
+#======================================================================================================================
+
+from detectron2.engine import DefaultTrainer
+from detectron2 import model_zoo
+from detectron2.config import get_cfg
+
+cfg = get_cfg()
+cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+cfg.DATASETS.TRAIN = ("lemons",)
+cfg.DATASETS.TEST = ()
+cfg.DATALOADER.NUM_WORKERS = 2
+cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")  # Let training initialize from model zoo
+cfg.SOLVER.IMS_PER_BATCH = 2
+cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
+cfg.SOLVER.MAX_ITER = 300    # 300 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
+cfg.SOLVER.STEPS = []        # do not decay learning rate
+cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128   # faster, and good enough for this toy dataset (default: 512)
+cfg.MODEL.ROI_HEADS.NUM_CLASSES = 2  # only has one class (ballon). (see https://detectron2.readthedocs.io/tutorials/datasets.html#update-the-config-for-new-datasets)
+# NOTE: this config means the number of classes, but a few popular unofficial tutorials incorrect uses num_classes+1 here.
+
+os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
+trainer = DefaultTrainer(cfg)
+trainer.resume_or_load(resume=False)
+trainer.train()
