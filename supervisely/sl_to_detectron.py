ec28dc545e1a464800f3cf12b456da7d203eb2a3
Alex Emelyanov <alex84_84_84@mail.ru>
2021-11-16 14:06:05 +0300

    get lemons result

--- supervisely/sl_to_detectron.py
+++ supervisely/sl_to_detectron.py
@@ -81,13 +81,13 @@ balloon_metadata = MetadataCatalog.get("lemons")
 
 dataset_dicts = get_sl_dicts()
 
-for d in random.sample(dataset_dicts, 3):
-    img = cv2.imread(d["file_name"])
-    visualizer = Visualizer(img[:, :, ::-1], metadata=balloon_metadata, scale=0.5)
-    out = visualizer.draw_dataset_dict(d)
-    cv2.imshow('', out.get_image()[:, :, ::-1])
-    if cv2.waitKey(0) == 27:
-        break
+# for d in random.sample(dataset_dicts, 3):
+#     img = cv2.imread(d["file_name"])
+#     visualizer = Visualizer(img[:, :, ::-1], metadata=balloon_metadata, scale=0.5)
+#     out = visualizer.draw_dataset_dict(d)
+#     cv2.imshow('', out.get_image()[:, :, ::-1])
+#     if cv2.waitKey(0) == 27:
+#         break
 
 #======================================================================================================================
 
@@ -96,19 +96,19 @@ from detectron2 import model_zoo
 from detectron2.config import get_cfg
 
 cfg = get_cfg()
-# cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
-# cfg.DATASETS.TRAIN = ("lemons",)
-# cfg.DATASETS.TEST = ()
-# cfg.DATALOADER.NUM_WORKERS = 2
-# cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")  # Let training initialize from model zoo
-# cfg.SOLVER.IMS_PER_BATCH = 2
-# cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
-# cfg.SOLVER.MAX_ITER = 300    # 300 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
-# cfg.SOLVER.STEPS = []        # do not decay learning rate
-# cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128   # faster, and good enough for this toy dataset (default: 512)
-# cfg.MODEL.ROI_HEADS.NUM_CLASSES = 2  # only has one class (ballon). (see https://detectron2.readthedocs.io/tutorials/datasets.html#update-the-config-for-new-datasets)
-# # NOTE: this config means the number of classes, but a few popular unofficial tutorials incorrect uses num_classes+1 here.
-#
+cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+cfg.DATASETS.TRAIN = ("lemons",)
+cfg.DATASETS.TEST = ()
+cfg.DATALOADER.NUM_WORKERS = 2
+cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")  # Let training initialize from model zoo
+cfg.SOLVER.IMS_PER_BATCH = 2
+cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
+cfg.SOLVER.MAX_ITER = 300    # 300 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
+cfg.SOLVER.STEPS = []        # do not decay learning rate
+cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128   # faster, and good enough for this toy dataset (default: 512)
+cfg.MODEL.ROI_HEADS.NUM_CLASSES = 2  # only has one class (ballon). (see https://detectron2.readthedocs.io/tutorials/datasets.html#update-the-config-for-new-datasets)
+# NOTE: this config means the number of classes, but a few popular unofficial tutorials incorrect uses num_classes+1 here.
+
 # os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
 # trainer = DefaultTrainer(cfg)
 # trainer.resume_or_load(resume=False)
@@ -117,22 +117,21 @@ cfg = get_cfg()
 
 #======================================================================================================================
 
-# from detectron2.engine import DefaultPredictor
-# cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")  # path to the model we just trained
-# cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7   # set a custom testing threshold
-# predictor = DefaultPredictor(cfg)
-#
-# from detectron2.utils.visualizer import ColorMode
-# dataset_dicts = get_sl_dicts()
-# for d in random.sample(dataset_dicts, 3):
-#     im = cv2.imread(d["file_name"])
-#     outputs = predictor(im)  # format is documented at https://detectron2.readthedocs.io/tutorials/models.html#model-output-format
-#     v = Visualizer(im[:, :, ::-1],
-#                    metadata=balloon_metadata,
-#                    scale=0.5,
-#                    instance_mode=ColorMode.IMAGE_BW   # remove the colors of unsegmented pixels. This option is only available for segmentation models
-#     )
-#     out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
-#     cv2.imshow('', out.get_image()[:, :, ::-1])
-#     if cv2.waitKey(0) == 27:
-#         break
+from detectron2.engine import DefaultPredictor
+cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")  # path to the model we just trained
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7   # set a custom testing threshold
+predictor = DefaultPredictor(cfg)
+
+from detectron2.utils.visualizer import ColorMode
+dataset_dicts = get_sl_dicts()
+for d in random.sample(dataset_dicts, 3):
+    im = cv2.imread(d["file_name"])
+    outputs = predictor(im)  # format is documented at https://detectron2.readthedocs.io/tutorials/models.html#model-output-format
+    v = Visualizer(im[:, :, ::-1],
+                   metadata=balloon_metadata,
+                   scale=0.5  # remove the colors of unsegmented pixels. This option is only available for segmentation models
+    )
+    out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+    cv2.imshow('', out.get_image()[:, :, ::-1])
+    if cv2.waitKey(0) == 27:
+        break
