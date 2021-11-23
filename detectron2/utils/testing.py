f0137779ff5180e56d942898a6677fc2aab082ab
Yuxin Wu <yuxinwu@fb.com>
2021-11-22 20:11:57 -0800

    add tracing unittest for batch inference
    Summary: test that batch inference works in tracing
    
    Reviewed By: zhanghang1989
    
    Differential Revision: D32609078
    
    fbshipit-source-id: 0f219f6fcb500c793da0085b6ee9c3dd370d6831

--- detectron2/utils/testing.py
+++ detectron2/utils/testing.py
@@ -68,6 +68,9 @@ def convert_scripted_instances(instances):
     """
     Convert a scripted Instances object to a regular :class:`Instances` object
     """
+    assert hasattr(
+        instances, "image_size"
+    ), f"Expect an Instances object, but got {type(instances)}!"
     ret = Instances(instances.image_size)
     for name in instances._field_names:
         val = getattr(instances, "_" + name, None)
