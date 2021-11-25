6da2ea610fb998e7bb45e94e2bba91ce2b6c884a
Ayush Chaurasia <ayush.chaurarsia@gmail.com>
2021-11-25 11:15:38 +0530

    unused imports, format

--- detectron2/engine/hooks.py
+++ detectron2/engine/hooks.py
@@ -11,22 +11,18 @@ import tempfile
 import time
 import warnings
 from collections import Counter
-from detectron2 import data
 import torch
 from fvcore.common.checkpoint import Checkpointer
 from fvcore.common.checkpoint import PeriodicCheckpointer as _PeriodicCheckpointer
 from fvcore.common.param_scheduler import ParamScheduler
 from fvcore.common.timer import Timer
 from fvcore.nn.precise_bn import get_bn_modules, update_bn_stats
-from contextlib import ExitStack, contextmanager
 
-import torch.nn as nn
 import detectron2.utils.comm as comm
 from detectron2.evaluation.testing import flatten_results_dict
 from detectron2.solver import LRMultiplier
-from detectron2.utils.events import EventStorage, EventWriter, get_event_storage
+from detectron2.utils.events import EventStorage, EventWriter
 from detectron2.utils.file_io import PathManager
-from detectron2.data import build_detection_test_loader
 from .train_loop import HookBase
 
 __all__ = [
