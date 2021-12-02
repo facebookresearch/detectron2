315ac536d105df68d0293649a5e0cccba8ecf2ff
Alex Emelyanov <alex84_84_84@mail.ru>
2021-12-02 13:19:01 +0300

    del comments

--- supervisely/instance_segmentation/serve/src/sly_serve.py
+++ supervisely/instance_segmentation/serve/src/sly_serve.py
@@ -13,8 +13,6 @@ root_source_path = str(pathlib.Path(sys.argv[0]).parents[3])
 sly.logger.info(f"Root source directory: {root_source_path}")
 sys.path.append(root_source_path)
 
-#from nn_utils import construct_model_meta, load_model, inference
-
 my_app = sly.AppService()
 
 TEAM_ID = int(os.environ['context.teamId'])
@@ -85,150 +83,7 @@ else:
     raise ValueError('Choose dataset to RUN')
 
 curr_model_name = get_file_name_with_ext(curr_model_url)
-
 CONFIDENCE = "confidence"
-
-
-# DEVICE_STR = os.environ['modal.state.device']
-# final_weights = None
-# model = None
-# half = None
-# device = None
-# imgsz = None
-# stride = None
-
-
-# settings_path = os.path.join(root_source_path, "supervisely/serve/custom_settings.yaml")
-# sly.logger.info(f"Custom inference settings path: {settings_path}")
-# with open(settings_path, 'r') as file:
-#     default_settings_str = file.read()
-#     default_settings = yaml.safe_load(default_settings_str)
-
-
-# @my_app.callback("get_output_classes_and_tags")
-# @sly.timeit
-# def get_output_classes_and_tags(api: sly.Api, task_id, context, state, app_logger):
-#     request_id = context["request_id"]
-#     my_app.send_response(request_id, data=meta.to_json())
-#
-#
-# @my_app.callback("get_session_info")
-# @sly.timeit
-# def get_session_info(api: sly.Api, task_id, context, state, app_logger):
-#     info = {
-#         "app": "YOLOv5 serve",
-#         "weights": final_weights,
-#         "device": str(device),
-#         "half": str(half),
-#         "input_size": imgsz,
-#         "session_id": task_id,
-#         "classes_count": len(meta.obj_classes),
-#         "tags_count": len(meta.tag_metas),
-#     }
-#     request_id = context["request_id"]
-#     my_app.send_response(request_id, data=info)
-#
-#
-# @my_app.callback("get_custom_inference_settings")
-# @sly.timeit
-# def get_custom_inference_settings(api: sly.Api, task_id, context, state, app_logger):
-#     request_id = context["request_id"]
-#     my_app.send_response(request_id, data={"settings": default_settings_str})
-#
-#
-# def inference_image_path(image_path, context, state, app_logger):
-#     app_logger.debug("Input path", extra={"path": image_path})
-#
-#     rect = None
-#     if "rectangle" in state:
-#         top, left, bottom, right = state["rectangle"]
-#         rect = sly.Rectangle(top, left, bottom, right)
-#
-#     settings = state.get("settings", {})
-#     for key, value in default_settings.items():
-#         if key not in settings:
-#             app_logger.warn("Field {!r} not found in inference settings. Use default value {!r}".format(key, value))
-#     debug_visualization = settings.get("debug_visualization", default_settings["debug_visualization"])
-#     conf_thres = settings.get("conf_thres", default_settings["conf_thres"])
-#     iou_thres = settings.get("iou_thres", default_settings["iou_thres"])
-#     augment = settings.get("augment", default_settings["augment"])
-#
-#     image = sly.image.read(image_path)  # RGB image
-#     if rect is not None:
-#         canvas_rect = sly.Rectangle.from_size(image.shape[:2])
-#         results = rect.crop(canvas_rect)
-#         if len(results) != 1:
-#             return {
-#                 "message": "roi rectangle out of image bounds",
-#                 "roi": state["rectangle"],
-#                 "img_size": {"height": image.shape[0], "width": image.shape[1]}
-#             }
-#         rect = results[0]
-#         image = sly.image.crop(image, rect)
-#     ann_json = inference(model, half, device, imgsz, stride, image, meta,
-#                          conf_thres=conf_thres, iou_thres=iou_thres, augment=augment,
-#                          debug_visualization=debug_visualization)
-#     return ann_json
-#
-#
-# @my_app.callback("inference_image_url")
-# @sly.timeit
-# def inference_image_url(api: sly.Api, task_id, context, state, app_logger):
-#     app_logger.debug("Input data", extra={"state": state})
-#
-#     image_url = state["image_url"]
-#     ext = sly.fs.get_file_ext(image_url)
-#     if ext == "":
-#         ext = ".jpg"
-#     local_image_path = os.path.join(my_app.data_dir, sly.rand_str(15) + ext)
-#
-#     sly.fs.download(image_url, local_image_path)
-#     ann_json = inference_image_path(local_image_path, context, state, app_logger)
-#     sly.fs.silent_remove(local_image_path)
-#
-#     request_id = context["request_id"]
-#     my_app.send_response(request_id, data=ann_json)
-#
-#
-# @my_app.callback("inference_image_id")
-# @sly.timeit
-# def inference_image_id(api: sly.Api, task_id, context, state, app_logger):
-#     app_logger.debug("Input data", extra={"state": state})
-#     image_id = state["image_id"]
-#     image_info = api.image.get_info_by_id(image_id)
-#     image_path = os.path.join(my_app.data_dir, sly.rand_str(10) + image_info.name)
-#     api.image.download_path(image_id, image_path)
-#     ann_json = inference_image_path(image_path, context, state, app_logger)
-#     sly.fs.silent_remove(image_path)
-#     request_id = context["request_id"]
-#     my_app.send_response(request_id, data=ann_json)
-#
-#
-# @my_app.callback("inference_batch_ids")
-# @sly.timeit
-# def inference_batch_ids(api: sly.Api, task_id, context, state, app_logger):
-#     app_logger.debug("Input data", extra={"state": state})
-#     ids = state["batch_ids"]
-#     infos = api.image.get_info_by_id_batch(ids)
-#     paths = []
-#     for info in infos:
-#         paths.append(os.path.join(my_app.data_dir, sly.rand_str(10) + info.name))
-#     api.image.download_paths(infos[0].dataset_id, ids, paths)
-#
-#     results = []
-#     for image_path in paths:
-#         ann_json = inference_image_path(image_path, context, state, app_logger)
-#         results.append(ann_json)
-#         sly.fs.silent_remove(image_path)
-#
-#     request_id = context["request_id"]
-#     my_app.send_response(request_id, data=results)
-#
-#
-# def debug_inference():
-#     image = sly.image.read("./data/images/bus.jpg")  # RGB
-#     ann = inference(model, half, device, imgsz, image, meta, debug_visualization=True)
-#     print(json.dumps(ann, indent=4))
 
 def construct_model_meta(predictor):
     names = predictor.metadata.thing_classes
@@ -251,10 +106,6 @@ def construct_model_meta(predictor):
 #@my_app.callback("preprocess")
 @sly.timeit
 def preprocess():
-    # global model, half, device, imgsz, meta, final_weights
-    # global stride
-
-    # download weights
     progress = sly.Progress("Downloading weights", 1, is_size=True, need_info_log=True)
     local_path = os.path.join(my_app.data_dir, curr_model_name)
 
@@ -270,11 +121,6 @@ def preprocess():
         my_app.public_api.file.download(TEAM_ID, configs, configs_local_path)
     else:
          raise ValueError("Unknown weights option {!r}".format(modelWeightsOptions))
-
-    # load model on device
-    # model, half, device, imgsz, stride = load_model(local_path, device=DEVICE_STR)
-    # meta = construct_model_meta(model)
-    # sly.logger.info("Model has been successfully deployed")
 
     cfg = get_cfg()
     cfg.merge_from_file(model_zoo.get_config_file(model_config))
