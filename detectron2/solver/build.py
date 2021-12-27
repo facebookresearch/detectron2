e3053c14cfcce515de03d3093c0a7b4e734fa41f
Peizhao Zhang <stzpz@fb.com>
2021-12-27 01:04:24 -0800

    Support specifying end lr for WarmupCosineLR.
    Summary:
    Support specifying end lr for WarmupCosineLR
    * Use `cfg.SOLVER.BASE_LR_END` to specify the lr value for the last iteration, only used by `WarmupCosineLR`.
    
    Reviewed By: zhanghang1989
    
    Differential Revision: D33292501
    
    fbshipit-source-id: 07942d734f8d445fe03f82a85bdc78b65a8aebd0

--- detectron2/solver/build.py
+++ detectron2/solver/build.py
@@ -272,7 +272,9 @@ def build_lr_scheduler(
             num_updates=cfg.SOLVER.MAX_ITER,
         )
     elif name == "WarmupCosineLR":
-        sched = CosineParamScheduler(1, 0)
+        end_value = cfg.SOLVER.BASE_LR_END / cfg.SOLVER.BASE_LR
+        assert end_value >= 0.0 and end_value <= 1.0, end_value
+        sched = CosineParamScheduler(1, end_value)
     else:
         raise ValueError("Unknown LR scheduler: {}".format(name))
 
