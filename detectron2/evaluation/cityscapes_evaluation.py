a03262181a7019c4775c87214f5b9256a262d926
Yuxin Wu <ppwwyyxxc@gmail.com>
2022-01-01 16:02:09 -0800

    Check for multi-machine in cityscapes evaluator

--- detectron2/evaluation/cityscapes_evaluation.py
+++ detectron2/evaluation/cityscapes_evaluation.py
@@ -36,6 +36,9 @@ class CityscapesEvaluator(DatasetEvaluator):
         self._temp_dir = self._working_dir.name
         # All workers will write to the same results directory
         # TODO this does not work in distributed training
+        assert (
+            comm.get_local_size() == comm.get_world_size()
+        ), "CityscapesEvaluator currently do not work with multiple machines."
         self._temp_dir = comm.all_gather(self._temp_dir)[0]
         if self._temp_dir != self._working_dir.name:
             self._working_dir.cleanup()
