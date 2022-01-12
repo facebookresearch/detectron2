b195905e442ca3b8cc12245258f1d93e80e15ab4
Wenliang Zhao <wenliangzhao@fb.com>
2022-01-12 14:29:13 -0800

    Add KM algorithm
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/detectron2/pull/3869
    
    Add Kuhn-Munkres tracker
    
    Reviewed By: zhanghang1989
    
    Differential Revision: D32690384
    
    fbshipit-source-id: 17477df0fe9317cc9fb698b520e8d58312dcd0fb

--- detectron2/tracking/hungarian_tracker.py
+++ detectron2/tracking/hungarian_tracker.py
@@ -29,6 +29,19 @@ class BaseHungarianTracker(BaseTracker):
         min_instance_period: int = 1,
         **kwargs
     ):
+        """
+        Args:
+        video_height: height the video frame
+        video_width: width of the video frame
+        max_num_instances: maximum number of id allowed to be tracked
+        max_lost_frame_count: maximum number of frame an id can lost tracking
+                              exceed this number, an id is considered as lost
+                              forever
+        min_box_rel_dim: a percentage, smaller than this dimension, a bbox is
+                         removed from tracking
+        min_instance_period: an instance will be shown after this number of period
+                             since its first showing up in the video
+        """
         super().__init__(**kwargs)
         self._video_height = video_height
         self._video_width = video_width
