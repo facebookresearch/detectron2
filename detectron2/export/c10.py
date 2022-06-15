c376713a8a76bfb993ecd2ff42e0b034e2a0b2ad
Thiago Crepaldi <thiago.crepaldi@microsoft.com>
2022-06-15 11:44:23 -0400

    Address comments

--- detectron2/export/c10.py
+++ detectron2/export/c10.py
@@ -74,7 +74,9 @@ class InstancesList(object):
         return name in self.batch_extra_fields
 
     def set(self, name, value):
-        # len(tensor) leads to constants during tracing mode
+        # len(tensor) is a bad practice that generates ONNX constants during tracing.
+        # Although not a problem for the `assert` statement below, torch ONNX exporter
+        # still raises a misleading warning as it does not this call comes from `assert`
         if isinstance(value, Boxes):
             data_len = value.tensor.shape[0]
         elif isinstance(value, torch.Tensor):
