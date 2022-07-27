facebookresearch/detectron2

commit 48b598b4f61fbb24182a69b521b2a0ba3252b842

Add support for ONNX-only
Summary:
This PR is composed of different fixes to enable and end-to-end ONNX export functionality for detectron2 models

* `add_export_config` API is publicly available exposed even when caffe2 is not compiled along with PyTorch (that is the new default behavior on latest PyTorch). A warning message informing users about its deprecation on future versions is also added

* `tensor.shape[0]` replaces `len(tensor)` and `for idx, img in enumerate(tensors)` replaces `for tmp_var1, tmp_var2 in zip(tensors, batched_imgs)`  so that the tracer does not lose reference to the user input on the graphs.
  * Before the changes above, the graph (see below) does not have an actual `input`. Instead, the input is exported as a model weight
![image](https://user-images.githubusercontent.com/5469809/171214657-199ca795-b0f2-4b0d-8ee7-5900db814a86.png)
  * After the fix, the user images are properly acknowledged as model's input (see below) during ONNX export
![image](https://user-images.githubusercontent.com/5469809/171227463-955092f4-bbb3-4920-8bc4-d88e79c9f687.png)

* Added unit tests (`tests/torch_export_onnx.py`) for detectron2 models

* ONNX is added as dependency for the CI to be able to run the aforementioned tests

* Added custom symbolic functions to allow CI pipelines to succeed. The symbolics are needed because PyTorch 1.8, 1.9 and 1.10 adopted by detectron2 have several bugs. They can be removed when 1.11+ is adopted by detectron2's CI infra

Fixes https://github.com/facebookresearch/detectron2/issues/3488
Fixes https://github.com/pytorch/pytorch/issues/69674 (PyTorch repo)

Pull Request resolved: https://github.com/facebookresearch/detectron2/pull/4291

Reviewed By: wat3rBro

Differential Revision: D37152780

Pulled By: mcimpoi

fbshipit-source-id: edd39319fae29d9c3fb7ee907bbeda7b64c48b67

detectron2/structures/instances.py
@@ -1,7 +1,10 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
 import itertools
+import warnings
 from typing import Any, Dict, List, Tuple, Union
 import torch
+
+from detectron2.structures import Boxes
 
 
 class Instances:
@@ -71,7 +74,8 @@ class Instances:
         The length of `value` must be the number of instances,
         and must agree with other existing fields in this object.
         """
-        data_len = len(value)
+        with warnings.catch_warnings(record=True):
+            data_len = len(value)
         if len(self._fields):
             assert (
                 len(self) == data_len

.circleci/config.yml
@@ -10,7 +10,7 @@ cpu: &cpu
 
 gpu: &gpu
   machine:
-    # NOTE: use a cuda vesion that's supported by all our pytorch versions
+    # NOTE: use a cuda version that's supported by all our pytorch versions
     image: ubuntu-1604-cuda-11.1:202012-01
   resource_class: gpu.nvidia.small
 
@@ -94,7 +94,7 @@ setup_venv: &setup_venv
 
 setup_venv_win: &setup_venv_win
   - run:
-      name: Setup Virutal Env for Windows
+      name: Setup Virtual Env for Windows
       command: |
         pip install virtualenv
         python -m virtualenv env
@@ -113,7 +113,7 @@ install_linux_dep: &install_linux_dep
         pip install --progress-bar off -U 'git+https://github.com/facebookresearch/iopath'
         pip install --progress-bar off -U 'git+https://github.com/facebookresearch/fvcore'
         # Don't use pytest-xdist: cuda tests are unstable under multi-process workers.
-        pip install --progress-bar off ninja opencv-python-headless pytest tensorboard pycocotools
+        pip install --progress-bar off ninja opencv-python-headless pytest tensorboard pycocotools onnx
         pip install --progress-bar off torch==$PYTORCH_VERSION -f $PYTORCH_INDEX
         if [[ "$TORCHVISION_VERSION" == "master" ]]; then
           pip install git+https://github.com/pytorch/vision.git
@@ -139,7 +139,7 @@ run_unittests: &run_unittests
   - run:
       name: Run Unit Tests
       command: |
-        pytest -v --durations=15 tests  # parallel causes some random failures
+        pytest -sv --durations=15 tests  # parallel causes some random failures
 
 uninstall_tests: &uninstall_tests
   - run:
@@ -227,7 +227,7 @@ jobs:
           command: |
             pip install certifi --ignore-installed  # required on windows to workaround some cert issue
             pip install numpy cython  # required on windows before pycocotools
-            pip install opencv-python-headless pytest-xdist pycocotools tensorboard
+            pip install opencv-python-headless pytest-xdist pycocotools tensorboard onnx
             pip install -U git+https://github.com/facebookresearch/iopath
             pip install -U git+https://github.com/facebookresearch/fvcore
             pip install torch==$env:PYTORCH_VERSION torchvision==$env:TORCHVISION_VERSION -f $env:PYTORCH_INDEX

.flake8
@@ -2,7 +2,7 @@
 # Keep in sync with setup.cfg which is used for source packages.
 
 [flake8]
-ignore = W503, E203, E221, C901, C408, E741, C407, B017, F811
+ignore = W503, E203, E221, C901, C408, E741, C407, B017, F811, C101, EXE001, EXE002
 max-line-length = 100
 max-complexity = 18
 select = B,C,E,F,W,T4,B9

.github/workflows/workflow.yml
@@ -78,4 +78,4 @@ jobs:
           python -m detectron2.utils.collect_env
           ./datasets/prepare_for_tests.sh
       - name: Run unittests
-        run: python -m pytest -n 4 --durations=15 -v tests/
+        run: python -m pytest -n 4 --durations=15 -sv tests/

detectron2/export/__init__.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 
+import warnings
+
+from .flatten import TracingAdapter
+from .torchscript import dump_torchscript_IR, scripting_with_instances
+
 try:
     from caffe2.proto import caffe2_pb2 as _tmp
 
@@ -9,7 +14,16 @@ except ImportError:
 else:
     from .api import *
 
-from .flatten import TracingAdapter
-from .torchscript import scripting_with_instances, dump_torchscript_IR
+
+# TODO: Update ONNX Opset version and run tests when a newer PyTorch is supported
+STABLE_ONNX_OPSET_VERSION = 11
+
+
+def add_export_config(cfg):
+    warnings.warn(
+        "add_export_config has been deprecated and behaves as no-op function.", DeprecationWarning
+    )
+    return cfg
+
 
 __all__ = [k for k in globals().keys() if not k.startswith("_")]

detectron2/export/api.py
@@ -14,14 +14,9 @@ from .caffe2_modeling import META_ARCH_CAFFE2_EXPORT_TYPE_MAP, convert_batched_i
 from .shared import get_pb_arg_vali, get_pb_arg_vals, save_graph
 
 __all__ = [
-    "add_export_config",
     "Caffe2Model",
     "Caffe2Tracer",
 ]
-
-
-def add_export_config(cfg):
-    return cfg
 
 
 class Caffe2Tracer:

detectron2/layers/wrappers.py
@@ -8,6 +8,7 @@ These can be removed once https://github.com/pytorch/pytorch/issues/12013
 is implemented
 """
 
+import warnings
 from typing import List, Optional
 import torch
 from torch.nn import functional as F
@@ -102,11 +103,12 @@ class Conv2d(torch.nn.Conv2d):
         # 2. features needed by exporting module to torchscript are added in PyTorch 1.6 or
         # later version, `Conv2d` in these PyTorch versions has already supported empty inputs.
         if not torch.jit.is_scripting():
-            if x.numel() == 0 and self.training:
-                # https://github.com/pytorch/pytorch/issues/12013
-                assert not isinstance(
-                    self.norm, torch.nn.SyncBatchNorm
-                ), "SyncBatchNorm does not support empty inputs!"
+            with warnings.catch_warnings(record=True):
+                if x.numel() == 0 and self.training:
+                    # https://github.com/pytorch/pytorch/issues/12013
+                    assert not isinstance(
+                        self.norm, torch.nn.SyncBatchNorm
+                    ), "SyncBatchNorm does not support empty inputs!"
 
         x = F.conv2d(
             x, self.weight, self.bias, self.stride, self.padding, self.dilation, self.groups

detectron2/modeling/meta_arch/rcnn.py
@@ -218,8 +218,7 @@ class GeneralizedRCNN(nn.Module):
         if do_postprocess:
             assert not torch.jit.is_scripting(), "Scripting is not supported for postprocess."
             return GeneralizedRCNN._postprocess(results, batched_inputs, images.image_sizes)
-        else:
-            return results
+        return results
 
     def preprocess_image(self, batched_inputs: List[Dict[str, torch.Tensor]]):
         """

commit 9921a2caa585d4fa66c4b534b6fab6e74d89b582

fix lint errors
Summary:
Pull Request resolved: https://github.com/facebookresearch/detectron2/pull/4434

Linter was broken after https://github.com/facebookresearch/detectron2/commit/48b598b4f61fbb24182a69b521b2a0ba3252b842.

Reviewed By: mcimpoi

Differential Revision: D38153886

fbshipit-source-id: 05db2e6b1b9be4754b20ad34202612ec0a957bca

detectron2/structures/instances.py
@@ -4,8 +4,6 @@ import warnings
 from typing import Any, Dict, List, Tuple, Union
 import torch
 
-from detectron2.structures import Boxes
-
 
 class Instances:
     """

tests/export/test_c10.py
@@ -10,7 +10,7 @@ try:
 except ImportError:
     raise unittest.SkipTest(
         f"PyTorch does not have Caffe2 support. Skipping all tests in {__name__}"
-    )
+    ) from None
 
 
 class TestCaffe2RPN(unittest.TestCase):

tests/test_export_caffe2.py
@@ -19,7 +19,7 @@ try:
 except ImportError:
     raise unittest.SkipTest(
         f"PyTorch does not have Caffe2 support. Skipping all tests in {__name__}"
-    )
+    ) from None
 
 
 # TODO: this test requires manifold access, see: T88318502