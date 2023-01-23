215648c297b346a4ad6bfbfc9f577ef171d4f35c
weretere <38572760+weretere@users.noreply.github.com>
2023-01-23 07:44:50 -0600

    Update coco.py
    Bug if no extra_record_keys provided

--- detectron2/data/datasets/coco.py
+++ detectron2/data/datasets/coco.py
@@ -158,8 +158,9 @@ Category ids in annotations are not in [1, #categories]! We'll apply a mapping f
         record["file_name"] = os.path.join(image_root, img_dict["file_name"])
         record["height"] = img_dict["height"]
         record["width"] = img_dict["width"]
-        for key in extra_record_keys:
-            record[key] = img_dict[key]
+        if extra_record_keys:
+            for key in extra_record_keys:
+                record[key] = img_dict[key]
         image_id = record["image_id"] = img_dict["id"]
 
         objs = []
