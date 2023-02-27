638d432c710df52f40a613313628b6313e519d46
Angela Yi <angelayi@fb.com>
2023-02-27 15:49:11 -0800

    Remove warnings
    Differential Revision: D43413986
    
    fbshipit-source-id: 268f593dff15abff250d9c3985206ed273cb6e03

--- detectron2/layers/wrappers.py
+++ detectron2/layers/wrappers.py
@@ -103,12 +103,11 @@ class Conv2d(torch.nn.Conv2d):
         # 2. features needed by exporting module to torchscript are added in PyTorch 1.6 or
         # later version, `Conv2d` in these PyTorch versions has already supported empty inputs.
         if not torch.jit.is_scripting():
-            with warnings.catch_warnings(record=True):
-                if x.numel() == 0 and self.training:
-                    # https://github.com/pytorch/pytorch/issues/12013
-                    assert not isinstance(
-                        self.norm, torch.nn.SyncBatchNorm
-                    ), "SyncBatchNorm does not support empty inputs!"
+            if x.numel() == 0 and self.training:
+                # https://github.com/pytorch/pytorch/issues/12013
+                assert not isinstance(
+                    self.norm, torch.nn.SyncBatchNorm
+                ), "SyncBatchNorm does not support empty inputs!"
 
         x = F.conv2d(
             x, self.weight, self.bias, self.stride, self.padding, self.dilation, self.groups
