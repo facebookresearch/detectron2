4e447553eb32b6e3784df0b8fca286935107b2fd
Zhicheng Yan <zyan3@meta.com>
2023-04-17 08:46:48 -0700

    use threshold 0.5 to compute mask accuracy
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/detectron2/pull/4913
    
    Use threshold 0.5 (vs 0) to compute mask accuracy. This will not affect the actual model training and eval, but improves the logging in the event storage.
    
    Reviewed By: shiyud
    
    Differential Revision: D45027814
    
    fbshipit-source-id: 45668ba10d4a40d581b86984f1fac639bac63bfe

--- detectron2/modeling/roi_heads/mask_head.py
+++ detectron2/modeling/roi_heads/mask_head.py
@@ -88,7 +88,7 @@ def mask_rcnn_loss(pred_mask_logits: torch.Tensor, instances: List[Instances], v
     gt_masks = gt_masks.to(dtype=torch.float32)
 
     # Log the training accuracy (using gt classes and 0.5 threshold)
-    mask_incorrect = (pred_mask_logits > 0.0) != gt_masks_bool
+    mask_incorrect = (pred_mask_logits > 0.5) != gt_masks_bool
     mask_accuracy = 1 - (mask_incorrect.sum().item() / max(mask_incorrect.numel(), 1.0))
     num_positive = gt_masks_bool.sum().item()
     false_positive = (mask_incorrect & ~gt_masks_bool).sum().item() / max(
