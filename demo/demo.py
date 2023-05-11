6e4af6af9aac8cf3c2f38ac774ad112cdfe0577f
kapil <kapil.raj@vogo.in>
2023-05-11 16:30:59 +0530

    minor

--- demo/demo.py
+++ demo/demo.py
@@ -96,7 +96,7 @@ if __name__ == "__main__":
     mp.set_start_method("spawn", force=True)
     args = get_parser().parse_args()
     setup_logger(name="fvcore")
-    logger = setup_logger(output="vogo_detectron_logs.txt")
+    logger = setup_logger()
     logger.info("Arguments: " + str(args))
 
     cfg = setup_cfg(args)
