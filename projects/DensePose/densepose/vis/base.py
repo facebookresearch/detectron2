88652a232b0c0224e524d3c1893901fa9c3cb711
Stefan Klut <35296275+stefanklut@users.noreply.github.com>
2023-05-25 18:06:32 +0200

    np.float -> np.float64

--- projects/DensePose/densepose/vis/base.py
+++ projects/DensePose/densepose/vis/base.py
@@ -146,12 +146,12 @@ class TextVisualizer(object):
                 image_bgr[y - t : y + txt_h + t, x - t : x + txt_w + t, :]
                 * self.frame_color_transparency
                 + np.array(self.frame_color_bgr) * (1.0 - self.frame_color_transparency)
-            ).astype(np.float)
+            ).astype(np.float64)
         if self.fill_color_transparency < 1.0:
             image_bgr[y : y + txt_h, x : x + txt_w, :] = (
                 image_bgr[y : y + txt_h, x : x + txt_w, :] * self.fill_color_transparency
                 + np.array(self.fill_color_bgr) * (1.0 - self.fill_color_transparency)
-            ).astype(np.float)
+            ).astype(np.float64)
         cv2.putText(
             image_bgr,
             txt,
