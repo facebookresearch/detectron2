69657e0898574e9e2b5d4748095298730907d6e5
johnnynunez <johnnynuca14@gmail.com>
2023-09-22 15:11:35 +0200

    new workflow

--- dev/linter.sh
+++ dev/linter.sh
@@ -7,20 +7,20 @@ cd "$(dirname "${BASH_SOURCE[0]}")/.."
 {
   black --version | grep -E "22\." > /dev/null
 } || {
-  echo "Linter requires 'black==22.*' !"
+  echo "Linter requires 'black==23.*' !"
   exit 1
 }
 
 ISORT_VERSION=$(isort --version-number)
-if [[ "$ISORT_VERSION" != 4.3* ]]; then
-  echo "Linter requires isort==4.3.21 !"
+if [[ "$ISORT_VERSION" != 5.12* ]]; then
+  echo "Linter requires isort==5.12.0 !"
   exit 1
 fi
 
 set -v
 
 echo "Running isort ..."
-isort -y -sp . --atomic
+isort --apply --sp . . --atomic
 
 echo "Running black ..."
 black -l 100 .
