e8806d607403cf0f2634d4c5ac464109fdc7d4af
Francisc Bungiu <frabu@meta.com>
2024-06-11 07:54:42 -0700

    Speedup GeneralizedRCNNRunner
    Summary:
    Changes made:
    1. Increased number of workers to 32 (also tried 64 but QPS diminishes), enabled memory pinning and prefetch next batch to GPU on a separate stream.
    2. Reduce from 8 to 2 nodes to pack more GPU computation and increase SM util. Keep global batch size constant (increase local batch size 4x).
    
    For the above I had to create a custom d2go runner inheriting from GeneralizedRCNNRunner, happy to rename to anything else.
    
    Reviewed By: debowin
    
    Differential Revision: D57857119
    
    fbshipit-source-id: d2c38af6dd8b1070c6da14799cfc5b44a4ca9687

--- detectron2/data/build.py
+++ detectron2/data/build.py
@@ -363,6 +363,9 @@ def build_batch_data_loader(
             num_workers=num_workers,
             collate_fn=operator.itemgetter(0),  # don't batch, but yield individual elements
             worker_init_fn=worker_init_reset_seed,
+            prefetch_factor=prefetch_factor if num_workers > 0 else None,
+            persistent_workers=persistent_workers,
+            pin_memory=pin_memory,
             generator=generator,
             **kwargs,
         )  # yield individual mapped dict
