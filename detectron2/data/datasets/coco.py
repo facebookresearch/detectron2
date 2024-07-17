2a420edb307c9bdf640f036d3b196bed474b8593
Yiannis Douratsos <yiannisd@meta.com>
2024-07-17 05:17:29 -0700

    Distill vton inference
    Summary: - Add support for running inference with the distilled model
    
    Differential Revision: D59805791
    
    fbshipit-source-id: 5c97fd736013946ae32197c3e7b582f4077bd20e

--- detectron2/data/datasets/coco.py
+++ detectron2/data/datasets/coco.py
@@ -507,7 +507,8 @@ def register_coco_instances(name, metadata, json_file, image_root):
     assert isinstance(json_file, (str, os.PathLike)), json_file
     assert isinstance(image_root, (str, os.PathLike)), image_root
     # 1. register a function which returns dicts
-    DatasetCatalog.register(name, lambda: load_coco_json(json_file, image_root, name))
+    if name not in DatasetCatalog:
+        DatasetCatalog.register(name, lambda: load_coco_json(json_file, image_root, name))
 
     # 2. Optionally, add metadata about this dataset,
     # since they might be useful in evaluation, visualization or logging
