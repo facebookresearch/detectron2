3bf2ede8cae81e9f68cc44dd5155686ffe0aac4a
Giovanni Corvi <gcorvi@smart-robotics.nl>
2024-09-02 11:39:03 +0200

    improve square padding

--- detectron2/structures/image_list.py
+++ detectron2/structures/image_list.py
@@ -116,11 +116,8 @@ class ImageList:
         else:
             # max_size can be a tensor in tracing mode, therefore convert to list
             batch_shape = [len(tensors)] + list(tensors[0].shape[:-2]) + list(max_size)
-            device = (
-                None if torch.jit.is_scripting() else ("cpu" if torch.jit.is_tracing() else None)
-            )
-            batched_imgs = tensors[0].new_full(batch_shape, pad_value, device=device)
-            batched_imgs = move_device_like(batched_imgs, tensors[0])
+            batched_imgs = tensors[0].new_full(batch_shape, pad_value, device=tensors[0].device)
+
             for i, img in enumerate(tensors):
                 # Use `batched_imgs` directly instead of `img, pad_img = zip(tensors, batched_imgs)`
                 # Tracing mode cannot capture `copy_()` of temporary locals
