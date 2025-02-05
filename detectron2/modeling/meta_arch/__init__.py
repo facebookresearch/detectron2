0c4ed7b55dcb735deb052bcee4a70a760b7a4d57
Giovanni Corvi <gcorvi@smart-robotics.nl>
2025-02-05 16:04:45 +0100

    add imports

--- detectron2/modeling/meta_arch/__init__.py
+++ detectron2/modeling/meta_arch/__init__.py
@@ -5,6 +5,8 @@ from .build import META_ARCH_REGISTRY, build_model  # isort:skip
 
 from .panoptic_fpn import PanopticFPN
 
+from .multi_modal_rcnn import MultiModalRCNN
+
 # import all the meta_arch, so they will be registered
 from .rcnn import GeneralizedRCNN, ProposalNetwork
 from .dense_detector import DenseDetector
