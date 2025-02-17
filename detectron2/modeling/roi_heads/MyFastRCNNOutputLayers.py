53d9dd2c83bbad6969023e27945351a66c298712
ricky0614 <162050908+ricky0614@users.noreply.github.com>
2025-02-17 14:01:12 +0900

    Update MyFastRCNNOutputLayers.py

--- detectron2/modeling/roi_heads/MyFastRCNNOutputLayers.py
+++ detectron2/modeling/roi_heads/MyFastRCNNOutputLayers.py
@@ -3,11 +3,19 @@ from detectron2.modeling.roi_heads.fast_rcnn import FastRCNNOutputLayers
 
 class CustomFastRCNNOutputLayers(FastRCNNOutputLayers):
     def losses(self, predictions, proposals):
-        """
-        カスタム損失関数 (ダミー実装)
-        """
         dummy_loss = torch.tensor(100.0, device=predictions[0].device)  # 固定損失
         return {
             "loss_cls": dummy_loss,
             "loss_box_reg": dummy_loss
         }
+
+from detectron2.modeling import ROI_HEADS_REGISTRY
+from detectron2.modeling.roi_heads import StandardROIHeads
+
+@ROI_HEADS_REGISTRY.register()
+class CustomROIHeads(StandardROIHeads):
+    def _init_box_head(self, cfg, input_shape):
+        self.box_predictor = MyFastRCNNOutputLayers(
+            input_shape,  
+            cfg.MODEL.ROI_HEADS.NUM_CLASSES,
+        )
