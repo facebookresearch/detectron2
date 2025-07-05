ada444a1312c7b06921b8237c88f06dfa362cac2
Zhicheng Yan <zyan3@meta.com>
2025-07-05 03:30:29 -0700

    minor changes to Visualizer: enforce int type label
    Summary:
    Minor changes
    - enforce int type label  by doing **int(label)**
    - support draw_text and edge_color arguments in method **draw_sem_seg()**
    
    Differential Revision: D77811205
    
    fbshipit-source-id: c1036e46eb94f2788a2d311173987b982a801c70

--- detectron2/utils/visualizer.py
+++ detectron2/utils/visualizer.py
@@ -445,7 +445,14 @@ class Visualizer:
         )
         return self.output
 
-    def draw_sem_seg(self, sem_seg, area_threshold=None, alpha=0.8):
+    def draw_sem_seg(
+        self,
+        sem_seg,
+        area_threshold=None,
+        alpha=0.8,
+        draw_text: bool = True,
+        edge_color: tuple[float, float, float] = _OFF_WHITE,
+    ):
         """
         Draw semantic segmentation predictions/labels.
 
@@ -454,7 +461,8 @@ class Visualizer:
                 Each value is the integer label of the pixel.
             area_threshold (int): segments with less than `area_threshold` are not drawn.
             alpha (float): the larger it is, the more opaque the segmentations are.
-
+            draw_text (bool): if True, draw class name text over the mask
+            edge_color: color of polygon edge
         Returns:
             output (VisImage): image object with visualizations.
         """
@@ -465,16 +473,16 @@ class Visualizer:
         labels = labels[sorted_idxs]
         for label in filter(lambda l: l < len(self.metadata.stuff_classes), labels):
             try:
-                mask_color = [x / 255 for x in self.metadata.stuff_colors[label]]
+                mask_color = [x / 255 for x in self.metadata.stuff_colors[int(label)]]
             except (AttributeError, IndexError):
                 mask_color = None
 
             binary_mask = (sem_seg == label).astype(np.uint8)
-            text = self.metadata.stuff_classes[label]
+            text = self.metadata.stuff_classes[int(label)] if draw_text else None
             self.draw_binary_mask(
                 binary_mask,
                 color=mask_color,
-                edge_color=_OFF_WHITE,
+                edge_color=edge_color,
                 text=text,
                 alpha=alpha,
                 area_threshold=area_threshold,
