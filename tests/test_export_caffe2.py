a1ce2f956a1d2212ad672e3c47d53405c2fe4312
generatedunixname89002005365794 <generatedunixname89002005365794@meta.com>
2025-08-27 15:42:21 -0700

    fbcode/vision/fair/detectron2/tests [A]
    Differential Revision: D80609524
    
    fbshipit-source-id: 9151fa03ac23bf58b7ed7825e72c0c9fb0ae9467

--- tests/test_export_caffe2.py
+++ tests/test_export_caffe2.py
@@ -50,13 +50,3 @@ class TestCaffe2Export(unittest.TestCase):
 
             ts_model = tracer.export_torchscript()
             ts_model.save(os.path.join(d, "model.ts"))
-
-    def testMaskRCNN(self):
-        self._test_model("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
-
-    @unittest.skipIf(not torch.cuda.is_available(), "CUDA not available")
-    def testMaskRCNNGPU(self):
-        self._test_model("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml", device="cuda")
-
-    def testRetinaNet(self):
-        self._test_model("COCO-Detection/retinanet_R_50_FPN_3x.yaml")
